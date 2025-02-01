6c30ba403bede3ea74ca53e218593d65320838ac
IdealistCat <sinconsistencia@gmail.com>
2025-02-01 17:42:30 -0600

    Stage 1 base file

---
+++ source/stages/stage1/Stage1.hx
@@ -0,0 +1,13 @@
+package stages.stage1;
+
+class Stage1 extends FlxState
+{
+    override function create() {
+        super.create();
+    }
+
+    override function update(elapsed:Float) {
+        super.update(elapsed);
+    }
+    
+}
