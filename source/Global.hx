64ebfb0cc09ff417562a2d47a2c12d12f59369dd
IdealistCat <sinconsistencia@gmail.com>
2025-01-31 20:43:44 -0600

    remove game build from version

--- source/Global.hx
+++ source/Global.hx
@@ -16,20 +16,10 @@ class Global
 	static function get_VERSION():String
 	{
 		var version = APPCURMETA.get('version');
-		var build = '';
 
-		if (DEBUG_BUILD)
-			build = '/#$GAME_BUILD';
-
-		return '${version}${build}';
+		return '${version}';
 	}
 
-	public static var GAME_BUILD(get, never):String;
-
-	static function get_GAME_BUILD():String
-	{
-		return APPCURMETA.get('build');
-	}
 	public static var DEBUG_BUILD(get, never):Bool;
 
 	static function get_DEBUG_BUILD():Bool
