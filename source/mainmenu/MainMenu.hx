1ee35c40f55e1e9bb29ca56f7fbd2a7780d7f2ac
IdealistCat <sinconsistencia@gmail.com>
2025-02-01 12:39:00 -0600

    menuboxtexts + sinco and port cycle between visible and blank + bg boppin (ig?)

--- source/mainmenu/MainMenu.hx
+++ source/mainmenu/MainMenu.hx
@@ -1,13 +1,20 @@
 package mainmenu;
 
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 
 class MainMenu extends FlxState
 {
-	var sinco:MenuCharacter = new MenuCharacter(0,0,"Sinco");
-	var port:MenuCharacter = new MenuCharacter(0,0,"Portilizen");
+	var sinco:MenuCharacter = new MenuCharacter(0, 0, "Sinco");
+	var port:MenuCharacter = new MenuCharacter(0, 0, "Portilizen");
+
 	var gridbg:FlxSprite = new FlxSprite();
 	var menuselectbox:FlxSprite = new FlxSprite();
+
+	var menuboxtexts:FlxTypedGroup<FlxText> = new FlxTypedGroup<FlxText>();
+	var menutexts:Map<String, Array<String>> = ['menu' => ['play', 'exit'], 'play' => ['new', 'continue', 'back']];
 
 	override function create()
 	{
@@ -26,17 +33,54 @@ class MainMenu extends FlxState
 
 		port.flipX = true;
 		add(port);
+		port.animation.play('visible');
 
 		menuselectbox.makeGraphic(64, 64, FlxColor.BLACK);
 		menuselectbox.scale.set(Global.DEFAULT_IMAGE_SCALE_MULTIPLIER, Global.DEFAULT_IMAGE_SCALE_MULTIPLIER);
 		menuselectbox.screenCenter();
 		add(menuselectbox);
 
+		set_menuboxtexts('menu');
+
+		add(menuboxtexts);
+
 		super.create();
 	}
 
+	var cycle:Int = 0;
+
 	override function update(elapsed:Float)
 	{
+		cycle++;
+
+		if (cycle % 100 == 0)
+		{
+			cycle = 0;
+			sinco.animation.play((sinco.animation.name == 'blank') ? 'visible' : 'blank');
+			port.animation.play((port.animation.name == 'blank') ? 'visible' : 'blank');
+
+			FlxTween.tween(gridbg, {
+				x: gridbg.x + ((port.animation.name == 'blank') ? 16 * Global.DEFAULT_IMAGE_SCALE_MULTIPLIER : -16 * Global.DEFAULT_IMAGE_SCALE_MULTIPLIER),
+				y: gridbg.y + ((port.animation.name == 'blank') ? 16 * Global.DEFAULT_IMAGE_SCALE_MULTIPLIER : -16 * Global.DEFAULT_IMAGE_SCALE_MULTIPLIER)
+			}, 1/4, {});
+		}
+
 		super.update(elapsed);
+	}
+
+	public function set_menuboxtexts(mapstring:String)
+	{
+		menuboxtexts.members = [];
+		var i = 0;
+		for (text in menutexts.get(mapstring))
+		{
+			var newtext:FlxText = new FlxText(menuselectbox.x, menuselectbox.y - menuselectbox.height + (i * 48), 0, text, 32);
+
+			newtext.alignment = CENTER;
+			newtext.ID = i;
+
+			menuboxtexts.add(newtext);
+			i++;
+		}
 	}
 }
