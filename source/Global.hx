a108f7eedbdb6561419aedaf7aa093afe68cfe43
IdealistCat <sinconsistencia@gmail.com>
2025-01-31 19:20:55 -0600

    APPCURMETA var so I dont constantly have to go "Application.current.meta"

--- source/Global.hx
+++ source/Global.hx
@@ -4,10 +4,17 @@ import lime.app.Application;
 
 class Global
 {
+	public static var APPCURMETA(get, never):Map<String, String>;
+
+	static function get_APPCURMETA():Map<String, String>
+	{
+		return Application.current.meta;
+	}
+
 	public static var VERSION(get, never):String;
 
 	static function get_VERSION():String
 	{
-		return Application.current.meta.get('version');
+		return APPCURMETA.get('version');
 	}
 }
