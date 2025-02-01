eaba2ac2d7108510b644ecd9543738285caa4558
IdealistCat <sinconsistencia@gmail.com>
2025-01-31 21:01:26 -0600

    Title State States (INTRO, FLASH, DONE)

--- source/title/TitleState.hx
+++ source/title/TitleState.hx
@@ -3,25 +3,61 @@ package title;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 
+enum abstract TitleStates(Int) from Int to Int
+{
+	var INTRO = 0;
+	var FLASH = 1;
+	var DONE = 2;
+}
+
 class TitleState extends FlxState
 {
-	var characterring:FlxSprite = new FlxSprite();
+	public var CURRENT_STATE:TitleStates = INTRO;
+
+	var charring:FlxSprite = new FlxSprite();
 
 	override public function create()
 	{
-		characterring.loadGraphic(FileManager.getImageFile('titlescreen/CharacterRing'));
-		characterring.scale.set(4, 4);
-		characterring.screenCenter(X);
-		characterring.y = -(characterring.height * 2);
-		add(characterring);
+		charring.loadGraphic(FileManager.getImageFile('titlescreen/CharacterRing'));
+		charring.scale.set(4, 4);
+		charring.screenCenter(X);
+		charring.y = -(charring.height * 2);
+		add(charring);
 
-		FlxTween.tween(characterring, {y: characterring.height + 16}, 1.0, {ease: FlxEase.sineOut});
 
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
+		stateChecks();
+
+		if (CURRENT_STATE == DONE) {}
+
 		super.update(elapsed);
+	}
+	public function stateChecks()
+	{
+		if (CURRENT_STATE == DONE)
+			return;
+
+		switch (CURRENT_STATE)
+		{
+			case INTRO:
+				FlxTween.tween(charring, {y: charring.height + 16}, 1.0, {
+					ease: FlxEase.sineOut,
+					onComplete: _tween ->
+					{
+						FlxG.camera.flash(0xFFFFFF, 4);
+						CURRENT_STATE = FLASH;
+					}
+				});
+
+			case FLASH:
+				Global.pass();
+
+			case DONE:
+				return;
+		}
 	}
 }
