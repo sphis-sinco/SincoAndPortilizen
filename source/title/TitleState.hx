a633e65b602c6d7961ab72abcaeba4cd04786b4a
IdealistCat <sinconsistencia@gmail.com>
2025-01-31 19:05:11 -0600

    TitleState file

---
+++ source/title/TitleState.hx
@@ -0,0 +1,14 @@
+package title;
+
+class TitleState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
