04439cd7bc78a7b64c295d616939a3e23b676bb6
IdealistCat <sinconsistencia@gmail.com>
2025-02-01 11:55:55 -0600

    Grid bg

--- source/mainmenu/MainMenu.hx
+++ source/mainmenu/MainMenu.hx
@@ -1,12 +1,20 @@
 package mainmenu;
 
+import flixel.addons.display.FlxGridOverlay;
+
 class MainMenu extends FlxState
 {
 	var sinco:MenuCharacter = new MenuCharacter(0,0,"Sinco");
 	var port:MenuCharacter = new MenuCharacter(0,0,"Portilizen");
+	var gridbg:FlxSprite = new FlxSprite();
 
 	override function create()
 	{
+		gridbg.loadGraphic(FileManager.getImageFile('mainmenu/MainMenuGrid'));
+		add(gridbg);
+		gridbg.scale.set(Global.DEFAULT_IMAGE_SCALE_MULTIPLIER, Global.DEFAULT_IMAGE_SCALE_MULTIPLIER);
+		gridbg.screenCenter();
+
 		sinco.screenCenter();
 		port.screenCenter();
 
