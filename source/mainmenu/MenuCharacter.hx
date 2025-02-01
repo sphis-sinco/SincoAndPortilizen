3d4cfc4d705d9ef3648bcaa4879e27a1ae3fd9e2
IdealistCat <sinconsistencia@gmail.com>
2025-02-01 11:07:54 -0600

    Main Menu Characters

---
+++ source/mainmenu/MenuCharacter.hx
@@ -0,0 +1,14 @@
+package mainmenu;
+
+class MenuCharacter extends FlxSprite
+{
+
+    override public function new(?X:Float = 0, ?Y:Float = 0, CHARSUFFIX:String = 'Sinco')
+    {
+        super(X,Y);
+        loadGraphic(FileManager.getImageFile('mainmenu/MainMenu$CHARSUFFIX'), true, 80, 96);
+        animation.add('visible', [0]);
+        animation.add('blank', [1]);
+        animation.play('blank');
+    }
+}
