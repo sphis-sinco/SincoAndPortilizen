508840f9a051152d820bc87a18ebf406f9935bf1
IdealistCat <sinconsistencia@gmail.com>
2025-01-31 19:06:02 -0600

    We will have an initState tho

---
+++ source/InitState.hx
@@ -0,0 +1,14 @@
+package;
+
+class InitState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
