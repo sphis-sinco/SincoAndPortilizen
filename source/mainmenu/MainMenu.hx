eb85ee06f63aa2bf3c3f0b3637385b4e450e3877
IdealistCat <sinconsistencia@gmail.com>
2025-02-01 23:24:04 -0600

    play music if its null in MainMenu

--- source/mainmenu/MainMenu.hx
+++ source/mainmenu/MainMenu.hx
@@ -65,6 +65,9 @@ class MainMenu extends FlxState
 
 		this.sinco.animation.play((menucharvis[0]) ? 'visible' : 'blank');
 		this.port.animation.play((menucharvis[1]) ? 'visible' : 'blank');
+		
+		if (FlxG.sound.music == null)
+			FlxG.sound.playMusic(FileManager.getSoundFile('music/22'), 1.0, true);
 
 		super.create();
 	}
