ca6a35ea3883d743fbfbc5ae0c6da1b1f0b87bdf
IdealistCat <sinconsistencia@gmail.com>
2025-02-01 18:09:53 -0600

    Stage 1 BG

--- source/stages/stage1/Stage1.hx
+++ source/stages/stage1/Stage1.hx
@@ -2,8 +2,19 @@ package stages.stage1;
 
 class Stage1 extends FlxState
 {
+    var background:FlxSprite = new FlxSprite();
+
     override function create() {
         super.create();
+
+        background.loadGraphic(FileManager.getImageFile('gameplay/sinco stages/Stage1BG'), true, 128, 128);
+
+        background.animation.add('animation', [0, 1], 16);
+        background.animation.play('animation');
+
+        background.scale.set(Global.DEFAULT_IMAGE_SCALE_MULTIPLIER + 1, Global.DEFAULT_IMAGE_SCALE_MULTIPLIER + 1);
+        background.screenCenter();
+        add(background);
     }
 
     override function update(elapsed:Float) {
