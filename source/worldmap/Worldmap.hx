bd0af2302af28310b5c490d57c4110d9eecbb807
IdealistCat <sinconsistencia@gmail.com>
2025-02-02 10:02:23 -0600

    map map

--- source/worldmap/Worldmap.hx
+++ source/worldmap/Worldmap.hx
@@ -4,6 +4,12 @@ class Worldmap extends FlxState
 {
     var character:MapCharacter = new MapCharacter();
 
+    var map:Map<Int, String> = [
+        1 => "stage1",
+        2 => "stage2",
+        3 => "stage3"
+    ];
+
     override function create() {
         super.create();
 
