9afc421fcbb06a7a7e0ad8944ffff4481a731189
IdealistCat <sinconsistencia@gmail.com>
2025-02-01 18:32:23 -0600

    sinco jumping

--- source/stages/stage1/Stage1.hx
+++ source/stages/stage1/Stage1.hx
@@ -1,31 +1,57 @@
 package stages.stage1;
 
+import flixel.math.FlxPoint;
+import flixel.tweens.FlxTween;
+
 class Stage1 extends FlxState
 {
-    var background:FlxSprite = new FlxSprite();
+	var background:FlxSprite = new FlxSprite();
 
-    var sinco:Sinco = new Sinco();
+	var sinco:Sinco = new Sinco();
 
-    override function create() {
-        super.create();
+	override function create()
+	{
+		super.create();
 
-        background.loadGraphic(FileManager.getImageFile('gameplay/sinco stages/Stage1BG'), true, 128, 128);
+		background.loadGraphic(FileManager.getImageFile('gameplay/sinco stages/Stage1BG'), true, 128, 128);
 
-        background.animation.add('animation', [0, 1], 16);
-        background.animation.play('animation');
+		background.animation.add('animation', [0, 1], 16);
+		background.animation.play('animation');
 
 		Global.scaleSprite(background, 1);
-        background.screenCenter();
-        add(background);
+		background.screenCenter();
+		add(background);
 
-        sinco.screenCenter();
-        sinco.y += sinco.height * 4;
-        sinco.x -= sinco.width * 4;
-        add(sinco);
-    }
+		sinco.screenCenter();
+		sinco.y += sinco.height * 4;
+		sinco.x -= sinco.width * 4;
+		add(sinco);
 
-    override function update(elapsed:Float) {
-        super.update(elapsed);
-    }
-    
+        sincoPos = new FlxPoint(0,0);
+		sincoPos.set(sinco.x, sinco.y);
+	}
+
+	var sincoPos:FlxPoint;
+    var sinco_jump_speed:Float = 0.5;
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.keys.justPressed.SPACE)
+		{
+			sinco.animation.play('jump');
+			FlxTween.tween(sinco, {x: 0, y: 0}, sinco_jump_speed, {
+				onComplete: _tween ->
+				{
+					FlxTween.tween(sinco, {x: sincoPos.x, y: sincoPos.y}, sinco_jump_speed, {
+						onComplete: _tween ->
+						{
+							sinco.animation.play('run');
+						}
+					});
+				}
+			});
+		}
+	}
 }
