5b4436c4ef2179a8338efd3cacabcb511841292a
IdealistCat <sinconsistencia@gmail.com>
2025-02-02 08:20:38 -0600

    Added ability to reset your gameplay status if you are holding R on startup

--- source/Main.hx
+++ source/Main.hx
@@ -7,6 +7,13 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+		
+		if (FlxG.keys.pressed.R)
+		{
+			trace('Gameplaystatus reset');
+			FlxG.save.data.gameplaystatus = null;
+		}
+
 		Global.change_saveslot(#if debug 'debug' #else 'release' #end);
 		addChild(new FlxGame(0, 0, InitState));
 	}
