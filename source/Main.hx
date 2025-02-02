3cae59ff97c8afecdde47eea2a785dab1322400d
IdealistCat <sinconsistencia@gmail.com>
2025-02-02 08:22:09 -0600

    removed feature

--- source/Main.hx
+++ source/Main.hx
@@ -7,13 +7,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		
-		if (FlxG.keys.pressed.R)
-		{
-			trace('Gameplaystatus reset');
-			FlxG.save.data.gameplaystatus = null;
-		}
-
 		Global.change_saveslot(#if debug 'debug' #else 'release' #end);
 		addChild(new FlxGame(0, 0, InitState));
 	}
