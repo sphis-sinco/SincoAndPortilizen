cf48a821fd753c7dc441d35120e1e2453ae0f8c1
IdealistCat <sinconsistencia@gmail.com>
2025-02-06 17:17:32 -0600

    Stage 4 Enemy

---
+++ source/stages/stage4/EnemyS4.hx
@@ -0,0 +1,14 @@
+package stages.stage4;
+
+class EnemyS4 extends FlxSprite
+{
+    override public function new() {
+        super();
+
+        loadGraphic(FileManager.getImageFile('gameplay/port stages/Stage4Enemy'), true, 32, 8);
+        animation.add('run', [0,1,2,1], 12);
+        animation.play('run');
+
+        Global.scaleSprite(this);
+    }
+}
