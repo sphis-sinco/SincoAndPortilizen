8da232fa1559f1d754b28964da6c28ce5b9c267b
IdealistCat <sinconsistencia@gmail.com>
2025-02-05 19:52:52 -0600

    Stage 4 file base

---
+++ source/stages/stage4/Stage4.hx
@@ -0,0 +1,17 @@
+package stages.stage4;
+
+class Stage4 extends FlxState
+{
+
+    override public function new() {
+        super();
+    }
+
+    override function create() {
+        super.create();
+    }
+
+    override function update(elapsed:Float) {
+        super.update(elapsed);
+    }
+}
