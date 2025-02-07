31486a79638b2fd3368db61f9621dae3a946f2de
IdealistCat <sinconsistencia@gmail.com>
2025-02-06 19:48:44 -0600

    Stage 4 Background implementation

--- source/stages/stage4/Stage4.hx
+++ source/stages/stage4/Stage4.hx
@@ -7,9 +7,16 @@ class Stage4 extends FlxState
 	var port:PortS4 = new PortS4();
 	var enemy:EnemyS4 = new EnemyS4();
 
+	var bg:FlxSprite = new FlxSprite();
+
 	override function create()
 	{
 		super.create();
+
+		bg.loadGraphic(FileManager.getImageFile('gameplay/port stages/Stage4BG'));
+		Global.scaleSprite(bg);
+		bg.screenCenter();
+		add(bg);
 
 		port.screenCenter();
 		port.x = FlxG.width - port.width * Global.DEFAULT_IMAGE_SCALE_MULTIPLIER * 4;
