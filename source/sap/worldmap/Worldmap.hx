6a267806382de67c27755bee0c1583bb6da1e620
IdealistCat <sinconsistencia@gmail.com>
2025-02-09 08:47:44 -0600

    Worldmap minor major optimizations

--- source/sap/worldmap/Worldmap.hx
+++ source/sap/worldmap/Worldmap.hx
@@ -17,7 +17,12 @@ class Worldmap extends FlxState
 
 	var mapGRP:FlxTypedGroup<FlxSprite> = new FlxTypedGroup<FlxSprite>();
 
-	var implementedLevels:Map<String, Array<Bool>> = ["sinco" => [true, false, false, false], "port" => [true, false, false, false]];
+	var implementedLevels(get, never):Map<String, Array<Bool>>;
+
+	function get_implementedLevels():Map<String, Array<Bool>>
+	{
+		return ["sinco" => [true, false, false, false], "port" => [true, false, false, false]];
+	}
 
 	override public function new(char:String = "Sinco")
 	{
@@ -59,63 +64,93 @@ class Worldmap extends FlxState
 
 		if (FlxG.keys.anyJustReleased([LEFT, RIGHT]) && character.animationname() != 'run')
 		{
-			canSwap = false;
-			character.flipX = FlxG.keys.justReleased.LEFT;
-
-			current_level += (character.flipX) ? -1 : 1;
-			if (current_level > 3)
-			{
-				current_level -= 1;
-				canSwap = true;
-				return;
-			}
-
-			character.animation.play('run');
-			FlxTween.tween(character, {x: character.x + ((character.flipX) ? -256 : 256)}, 1, {
-				onComplete: tween ->
-				{
-					canSwap = true;
-					character.animation.play('idle');
-
-					if (current_level < 1)
-					{
-						FlxG.switchState(() -> new MainMenu());
-						current_level += 1;
-					}
-				}
-			});
+			characterMove();
 		}
 
 		if (FlxG.keys.justReleased.ENTER && character.animationname() == 'idle')
 		{
-			switch (current_level)
-			{
-				// TODO: implement level unlocking PLEASE
-				case 1:
-					FlxG.switchState(() -> ((character.lowercase_char() == 'sinco') ? new Stage1() : new Stage4()));
-			}
+			playLevel();
 		}
 
 		if (FlxG.keys.justReleased.SPACE && canSwap)
 		{
-			canSwap = false;
-			charWheel.animation.play('${character.lowercase_char()}-${character.swappedchar().toLowerCase()}');
-
-			FlxTimer.wait(2 / 12, () ->
-			{
-				FlxG.camera.flash(0xffffff, 1, () ->
-				{
-					canSwap = true;
-				});
-				for (tile in mapGRP)
-				{
-					tile.destroy();
-					mapGRP.remove(tile);
-				}
-				makeMap();
-				character.swapCharacter();
-			});
+			swap();
 		}
+	}
+
+	public function characterMove()
+	{
+		canSwap = false;
+		character.flipX = FlxG.keys.justReleased.LEFT;
+
+		current_level += (character.flipX) ? -1 : 1;
+		if (current_level > 3)
+		{
+			current_level -= 1;
+			canSwap = true;
+			return;
+		}
+
+		character.animation.play('run');
+		FlxTween.tween(character, {x: character.x + ((character.flipX) ? -256 : 256)}, 1, {
+			onComplete: characterMoveDone()
+		});
+	}
+
+	public function characterMoveDone():TweenCallback
+	{
+		return tween ->
+		{
+			canSwap = true;
+			character.animation.play('idle');
+
+			if (current_level < 1)
+			{
+				FlxG.switchState(() -> new MainMenu());
+				current_level += 1;
+			}
+		}
+	}
+
+	public function playLevel()
+	{
+		switch (current_level)
+		{
+			// TODO: implement level unlocking PLEASE
+			case 1:
+				level1();
+		}
+	}
+
+	public function level1()
+	{
+		FlxG.switchState(() -> ((character.lowercase_char() == 'sinco') ? new Stage1() : new Stage4()));
+	}
+
+	public function swap()
+	{
+		canSwap = false;
+		charWheel.animation.play('${character.lowercase_char()}-${character.swappedchar().toLowerCase()}');
+
+		FlxTimer.wait(2 / 12, () ->
+		{
+			swapWaitDone();
+		});
+	}
+
+	public function swapWaitDone()
+	{
+		FlxG.camera.flash(0xffffff, 1, () ->
+		{
+			canSwap = true;
+		});
+		for (tile in mapGRP)
+		{
+			tile.destroy();
+			mapGRP.remove(tile);
+		}
+		makeMap();
+		character.swapCharacter();
 	}
 
 	var canSwap:Bool = true;
@@ -125,23 +160,28 @@ class Worldmap extends FlxState
 		var i = 0;
 		while (i < 3)
 		{
-			// TODO: change these to use MapTile once you figure out the bug
-
-			var level:FlxSprite = new FlxSprite(mapTileXPosThing - 12 + (i * 256), character.getGraphicMidpoint().y);
-			var tileColor:FlxColor = FlxColor.BLACK;
-
-			if (implementedLevels.get(character.lowercase_char())[i] != false)
-			{
-				tileColor = FlxColor.RED;
-
-				// TODO: implement color change for when a level is finished
-			}
-
-			level.makeGraphic(24, 24, tileColor);
-
-			mapGRP.add(level);
+			makeNewTile(i);
 
 			i++;
 		}
+	}
+
+	public function makeNewTile(i:Int)
+	{
+		// TODO: change these to use MapTile once you figure out the bug
+
+		var level:FlxSprite = new FlxSprite(mapTileXPosThing - 12 + (i * 256), character.getGraphicMidpoint().y);
+		var tileColor:FlxColor = FlxColor.BLACK;
+
+		if (implementedLevels.get(character.lowercase_char())[i] != false)
+		{
+			tileColor = FlxColor.RED;
+
+			// TODO: implement color change for when a level is finished
+		}
+
+		level.makeGraphic(24, 24, tileColor);
+
+		mapGRP.add(level);
 	}
 }
