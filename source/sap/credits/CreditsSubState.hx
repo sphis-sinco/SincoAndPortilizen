bff86678d2cb6f85d3c51467e1511c9ad19a7f7e
IdealistCat <sinconsistencia@gmail.com>
2025-02-14 16:12:10 -0600

    Credits SubState

---
+++ source/sap/credits/CreditsSubState.hx
@@ -0,0 +1,13 @@
+package sap.credits;
+
+class CreditsSubState extends FlxSubState
+{
+        override public function new() {
+                super();
+        }
+
+        override function create() {
+                super.create();
+        }
+        
+}
