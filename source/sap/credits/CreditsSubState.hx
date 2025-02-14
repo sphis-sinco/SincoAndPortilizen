31b8e682b2a84f12c66b04fc02881a00669ff222
IdealistCat <sinconsistencia@gmail.com>
2025-02-14 16:23:12 -0600

    Add Credits Overlay

--- source/sap/credits/CreditsSubState.hx
+++ source/sap/credits/CreditsSubState.hx
@@ -4,12 +4,18 @@ import sap.mainmenu.MainMenu;
 
 class CreditsSubState extends FlxSubState
 {
+        var overlay:BlankBG = new BlankBG();
+        
         override public function new() {
                 super();
         }
 
         override function create() {
                 super.create();
+
+                overlay.color = 0x000000;
+                overlay.alpha = 0.5;
+                add(overlay);
         }
 
         override function update(elapsed:Float) {
