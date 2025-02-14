c9f93ead548bf052f809d6f81fe3e8001cbd0312
IdealistCat <sinconsistencia@gmail.com>
2025-02-13 21:44:45 -0600

    ADV SPR Params (X and Y)

--- source/sap/adv_spr/AdvancedSprite.hx
+++ source/sap/adv_spr/AdvancedSprite.hx
@@ -1,10 +1,21 @@
 package sap.adv_spr;
 
+typedef AdvancedSpriteParams = {
+        public var X:Null<Float>;
+        public var Y:Null<Float>;
+}
+
 class AdvancedSprite extends FlxSprite
 {
+        public var params:AdvancedSpriteParams;
 
-        override public function new() {
-                super();
+        override public function new(params:AdvancedSpriteParams) {
+                this.params = params;
+
+                var posX:Float = (this.params.X != null) ? this.params.X : 0;
+                var posY:Float = (this.params.Y != null) ? this.params.Y : 0;
+
+                super(posX, posY);
         }
         
 }
