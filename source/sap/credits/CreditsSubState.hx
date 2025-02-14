73f718c53fd6d905a628a9f1d4675154d47d6f31
IdealistCat <sinconsistencia@gmail.com>
2025-02-14 16:13:05 -0600

    Leaving the SubState

--- source/sap/credits/CreditsSubState.hx
+++ source/sap/credits/CreditsSubState.hx
@@ -4,10 +4,22 @@ class CreditsSubState extends FlxSubState
 {
         override public function new() {
                 super();
+
+                trace('Inside the CSS');
         }
 
         override function create() {
                 super.create();
+        }
+
+        override function update(elapsed:Float) {
+                super.update(elapsed);
+
+                if (FlxG.keys.justReleased.ESCAPE)
+                {
+                        trace('Outside the CSS');
+                        closeSubState();
+                }
         }
         
 }
