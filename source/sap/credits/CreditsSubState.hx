0c3f84b89fa288f04d650994103bddacf3c5e08e
IdealistCat <sinconsistencia@gmail.com>
2025-02-14 16:33:19 -0600

    Implement actual credit texts

--- source/sap/credits/CreditsSubState.hx
+++ source/sap/credits/CreditsSubState.hx
@@ -2,6 +2,7 @@ package sap.credits;
 
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
+import flixel.util.FlxColor;
 import sap.mainmenu.MainMenu;
 
 class CreditsSubState extends FlxSubState
@@ -20,6 +21,23 @@ class CreditsSubState extends FlxSubState
                 add(overlay);
 
                 add(creditsText);
+
+                var cur_y:Float = 10;
+                for (credit in creditsJSON)
+                {
+                        var text:FlxText = new FlxText(0, cur_y, 0, credit.text, Std.int(32 * credit.size));
+                        text.screenCenter(X);
+                        text.color = FlxColor.fromRGB(
+                                credit.color[0],
+                                credit.color[1],
+                                credit.color[2], 
+                                (credit.color[3] != null) ? credit.color[3] : 255
+                                );
+
+                        creditsText.add(text);
+
+                        cur_y += credit.spacing;
+                }
         }
 
         override function update(elapsed:Float) {
