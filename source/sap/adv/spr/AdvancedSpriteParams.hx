e5f54bcc531d6cd274eb8e08f023797566a744c6
IdealistCat <sinconsistencia@gmail.com>
2025-02-13 22:09:26 -0600

    Revert "In theory this should work"
    This reverts commit 2c74c99b11c4744799c2aeef10950c5b09d5440e.

--- source/sap/adv/spr/AdvancedSpriteParams.hx
+++ source/sap/adv/spr/AdvancedSpriteParams.hx
@@ -6,14 +6,14 @@ typedef AdvancedSpriteParams =
 	public var Y:Null<Float>;
 	public var asset_path:String;
 	public var ?px_animated:Bool;
-	public var ?px_width:Int;
-	public var ?px_height:Int;
+	public var ?px_width:Float;
+	public var ?px_height:Float;
 	public var ?px_animations:Array<PX_Animations>;
 }
 
 typedef PX_Animations = {
         public var name:String;
-        public var frames:Array<Int>;
+        public var frames:Array<Float>;
         public var ?fps:Float;
         public var ?loop:Bool;
 }
