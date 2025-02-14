2c74c99b11c4744799c2aeef10950c5b09d5440e
IdealistCat <sinconsistencia@gmail.com>
2025-02-13 22:01:54 -0600

    In theory this should work

--- source/sap/adv/spr/AdvancedSprite.hx
+++ source/sap/adv/spr/AdvancedSprite.hx
@@ -11,6 +11,37 @@ class AdvancedSprite extends FlxSprite
                 var posY:Float = (this.params.Y != null) ? this.params.Y : 0;
 
                 super(posX, posY);
+
+                if (this.params.asset_path == null) throw "Missing required ADV_SPR_Params field: asset_path";
+                
+                if (this.params.px_animated)
+                {
+                        if (this.params.px_width == null) throw "Missing required ADV_SPR_Params field: px_width";
+                        if (this.params.px_height == null) throw "Missing required ADV_SPR_Params field: px_height";
+                        if (this.params.px_animations == null) throw "Missing required ADV_SPR_Params field: px_animations";
+
+                        var animated:Bool = this.params.px_animated;
+                        var px_w:Bool = this.params.px_width;
+                        var px_h:Bool = this.params.px_height;
+
+			loadGraphic(
+                                FileManager.getImageFile(this.params.asset_path), 
+                                animated, 
+                                (animated) ? px_w : 0, 
+                                (animated) ? px_h : 0
+                        );
+
+                        for (anim in this.params.px_animations)
+                        {
+                                if (anim.name == null) throw "Missing required px_animations field: name";
+                                if (anim.frames == null) throw "Missing required px_animations field: frames";
+
+                                var fps:Float = (anim.fps != null) ? anim.fps : 30;
+                                var loop:Float = (anim.loop != null) ? anim.loop : true;
+
+                                animation.add(anim.name, anim.frames, fps, loop);
+                        }
+                }
         }
         
 }
