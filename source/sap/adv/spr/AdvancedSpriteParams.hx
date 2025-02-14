c4e2e7942df8658c31e96728a1ecb4b6400d1d59
IdealistCat <sinconsistencia@gmail.com>
2025-02-13 21:53:42 -0600

    chaos emerald ADV SPR json

--- source/sap/adv/spr/AdvancedSpriteParams.hx
+++ source/sap/adv/spr/AdvancedSpriteParams.hx
@@ -1,6 +1,19 @@
 package sap.adv.spr;
 
-typedef AdvancedSpriteParams = {
-        public var X:Null<Float>;
-        public var Y:Null<Float>;
+typedef AdvancedSpriteParams =
+{
+	public var X:Null<Float>;
+	public var Y:Null<Float>;
+	public var asset_path:String;
+	public var ?px_animated:Bool;
+	public var ?px_width:Float;
+	public var ?px_height:Float;
+	public var ?px_animations:Array<PX_Animations>;
+}
+
+typedef PX_Animations = {
+        public var name:String;
+        public var frames:Array<Float>;
+        public var ?fps:Float;
+        public var ?loop:Bool;
 }
