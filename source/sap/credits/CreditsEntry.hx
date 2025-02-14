cf6029a1d3cf1123d8ec075c3fce230c2d77daa0
IdealistCat <sinconsistencia@gmail.com>
2025-02-14 16:31:07 -0600

    Null<Int>

--- source/sap/credits/CreditsEntry.hx
+++ source/sap/credits/CreditsEntry.hx
@@ -3,6 +3,6 @@ package sap.credits;
 typedef CreditsEntry = {
         public var text:String;
         public var size:Float;
-        public var color:Array<Int>;
+        public var color:Array<Null<Int>>;
         public var spacing:Int;
 }
