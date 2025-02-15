8d77ddb6cc7877740394672e7c1f222356a047df
IdealistCat <sinconsistencia@gmail.com>
2025-02-15 00:24:56 -0600

    Functions for create and update

--- source/sap/modding/source/ModListManager.hx
+++ source/sap/modding/source/ModListManager.hx
@@ -1,17 +1,41 @@
 package sap.modding.source;
 
-class ModListManager {
-        /**
-         * List of mod files
-         */
-        public static var modList:Array<ModBasic> = [];
+class ModListManager
+{
+	/**
+	 * List of mod files
+	 */
+	public static var modList:Array<ModBasic> = [];
 
-        /**
-         * Adds a mod to `modList`
-         * @param newmod the mod your adding
-         */
-        public static function addMod(newmod:ModBasic):Void
-        {
-                modList.push(newmod);
-        }
+	/**
+	 * Adds a mod to `modList`
+	 * @param newmod the mod your adding
+	 */
+	public static function addMod(newmod:ModBasic):Void
+	{
+		modList.push(newmod);
+	}
+
+	public static function create():Void
+	{
+		for (mod in modList)
+		{
+			mod.create();
+		}
+	}
+
+	/**
+	 * Runs every frame updating every mod.
+	 * @param elapsed elapsed
+	 */
+	public static function update(elapsed:Float):Void
+	{
+		for (mod in modList)
+		{
+			if (mod.enabled)
+			{
+				mod.update(elapsed);
+			}
+		}
+	}
 }
