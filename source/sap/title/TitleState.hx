83f19295a642f5fafdc6849c6fa8a0bafbfda59d
IdealistCat <sinconsistencia@gmail.com>
2025-02-15 01:02:00 -0600

    Revert "PS variables"
    This reverts commit 6d6be508972338ce727502ee665920a319dae12c.

--- source/sap/title/TitleState.hx
+++ source/sap/title/TitleState.hx
@@ -19,14 +19,14 @@ class TitleState extends State
 {
 	public static var CURRENT_STATE:TitleStates = INTRO;
 
-	public static var CHARRING:FlxSprite = new FlxSprite();
-	public static var PRESS_ANY_BUTTON:FlxSprite = new FlxSprite();
-	public static var TITLE_BG:FlxSprite = new FlxSprite();
+	var charring:FlxSprite = new FlxSprite();
+	var pressany:FlxSprite = new FlxSprite();
+	var titlebg:FlxSprite = new FlxSprite();
 
-	public static var SINCO:TitleSinco = new TitleSinco();
-	public static var PORTILIZEN:TitlePort = new TitlePort();
+	var sinco:TitleSinco = new TitleSinco();
+	var port:TitlePort = new TitlePort();
 
-	public static var VERSION_TEXT:FlxText = new FlxText();
+	var versiontext:FlxText = new FlxText();
 
         public static dynamic function get_versiontext():String
         {
@@ -35,42 +35,42 @@ class TitleState extends State
 
 	override public function create()
 	{
-		TITLE_BG.loadGraphic(FileManager.getImageFile('titlescreen/TITLE_BG'));
-		Global.scaleSprite(TITLE_BG, 2);
-		TITLE_BG.screenCenter(XY);
-		TITLE_BG.visible = false;
-		add(TITLE_BG);
+		titlebg.loadGraphic(FileManager.getImageFile('titlescreen/TitleBG'));
+		Global.scaleSprite(titlebg, 2);
+		titlebg.screenCenter(XY);
+		titlebg.visible = false;
+		add(titlebg);
 
-		CHARRING.loadGraphic(FileManager.getImageFile('titlescreen/CharacterRing'));
-		Global.scaleSprite(CHARRING, 0);
-		CHARRING.screenCenter(X);
-		CHARRING.y = -(CHARRING.height * 2);
-		add(CHARRING);
+		charring.loadGraphic(FileManager.getImageFile('titlescreen/CharacterRing'));
+		Global.scaleSprite(charring, 0);
+		charring.screenCenter(X);
+		charring.y = -(charring.height * 2);
+		add(charring);
 
-		PRESS_ANY_BUTTON.loadGraphic(FileManager.getImageFile('titlescreen/PRESS_ANY_BUTTONToPlay'));
-		Global.scaleSprite(PRESS_ANY_BUTTON, 0);
-		PRESS_ANY_BUTTON.screenCenter(XY);
-		PRESS_ANY_BUTTON.visible = false;
-		add(PRESS_ANY_BUTTON);
+		pressany.loadGraphic(FileManager.getImageFile('titlescreen/PressAnyToPlay'));
+		Global.scaleSprite(pressany, 0);
+		pressany.screenCenter(XY);
+		pressany.visible = false;
+		add(pressany);
 
-		Global.scaleSprite(SINCO, 0);
-		SINCO.visible = false;
-		add(SINCO);
+		Global.scaleSprite(sinco, 0);
+		sinco.visible = false;
+		add(sinco);
 
-		Global.scaleSprite(PORTILIZEN, 0);
-		PORTILIZEN.visible = false;
-		add(PORTILIZEN);
+		Global.scaleSprite(port, 0);
+		port.visible = false;
+		add(port);
 
 		if (CURRENT_STATE == INTRO)
 			Global.playSoundEffect('start-synth');
 
-		PRESS_ANY_BUTTONTargY = FlxG.height - (PRESS_ANY_BUTTON.height * 2) - (16 * 2);
+		pressanyTargY = FlxG.height - (pressany.height * 2) - (16 * 2);
 
-		VERSION_TEXT.size = 16;
-		VERSION_TEXT.setPosition(5, 5);
-		VERSION_TEXT.text = get_versiontext();
-		VERSION_TEXT.color = FlxColor.BLACK;
-		add(VERSION_TEXT);
+		versiontext.size = 16;
+		versiontext.setPosition(5, 5);
+		versiontext.text = get_versiontext();
+		versiontext.color = FlxColor.BLACK;
+		add(versiontext);
 
 		super.create();
                 
@@ -85,7 +85,7 @@ class TitleState extends State
 
 		if (CURRENT_STATE == DONE)
 		{
-			pressany();
+			pressAny();
 		}
 
 		super.update(elapsed);
@@ -113,7 +113,7 @@ class TitleState extends State
 
 	public function introState()
 	{
-		FlxTween.tween(CHARRING, {y: CHARRING.height + 16}, 1.0, {
+		FlxTween.tween(charring, {y: charring.height + 16}, 1.0, {
 			ease: FlxEase.sineOut,
 			onComplete: introStateDone()
 		});
@@ -135,16 +135,16 @@ class TitleState extends State
 	{
 		Global.playMenuMusic();
 
-		PRESS_ANY_BUTTON.y = PRESS_ANY_BUTTONTargY;
-		TITLE_BG.visible = true;
+		pressany.y = pressanyTargY;
+		titlebg.visible = true;
 
 		FlxTimer.wait(5, () ->
 		{
 			CURRENT_STATE = DONE;
-			PRESS_ANY_BUTTON.visible = true;
-			PRESS_ANY_BUTTON.alpha = 0;
+			pressany.visible = true;
+			pressany.alpha = 0;
 
-			FlxTween.tween(PRESS_ANY_BUTTON, {alpha: 1}, 1);
+			FlxTween.tween(pressany, {alpha: 1}, 1);
 		});
 	}
 
@@ -152,34 +152,34 @@ class TitleState extends State
 	{
 		Global.playMenuMusic();
 
-		if (PRESS_ANY_BUTTON.y != PRESS_ANY_BUTTONTargY)
-			PRESS_ANY_BUTTON.y = PRESS_ANY_BUTTONTargY;
-		if (!PRESS_ANY_BUTTON.visible)
-			PRESS_ANY_BUTTON.visible = true;
-		if (!TITLE_BG.visible)
-			TITLE_BG.visible = true;
-		if (CHARRING.y != CHARRING.height + 16)
-			CHARRING.y = CHARRING.height + 16;
+		if (pressany.y != pressanyTargY)
+			pressany.y = pressanyTargY;
+		if (!pressany.visible)
+			pressany.visible = true;
+		if (!titlebg.visible)
+			titlebg.visible = true;
+		if (charring.y != charring.height + 16)
+			charring.y = charring.height + 16;
 
-		randomBGChar(SINCO, 6);
-		randomBGChar(PORTILIZEN, 4);
+		randomBGChar(sinco, 6);
+		randomBGChar(port, 4);
 	}
 
-	public function pressany()
+	public function pressAny()
 	{
 		if (FlxG.keys.justReleased.ANY && !transitioning)
 		{
 			transitioning = !transitioning;
 			Global.playSoundEffect('blipSelect');
 			FlxG.camera.flash(0xFFFFFF, 2);
-			FlxFlicker.flicker(PRESS_ANY_BUTTON, 3, 0.04, true, true, _flicker ->
+			FlxFlicker.flicker(pressany, 3, 0.04, true, true, _flicker ->
 			{
 				FlxG.switchState(() -> new MainMenu());
 			});
 		}
 	}
 
-	var PRESS_ANY_BUTTONTargY:Float = 0;
+	var pressanyTargY:Float = 0;
 
 	public function randomBGChar(char:FlxSprite, chance:Float)
 	{
