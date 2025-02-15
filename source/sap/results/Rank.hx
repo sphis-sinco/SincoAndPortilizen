e899e77095e3e0d57ef3247f893c4be849554de6
IdealistCat <sinconsistencia@gmail.com>
2025-02-15 12:00:44 -0600

    gradeUntranslated

--- source/sap/results/Rank.hx
+++ source/sap/results/Rank.hx
@@ -11,12 +11,17 @@ class Rank
 
 	public function grade(percent:Float = 0.0):String
 	{
-		if (percent == 100) return PhraseManager.getPhrase('rank-perfect', 'perfect');
-		if (percent >= 90) return PhraseManager.getPhrase('rank-excellent', 'excellent');
-		if (percent >= 80) return PhraseManager.getPhrase('rank-great', 'great');
-		if (percent >= 60) return PhraseManager.getPhrase('rank-good', 'good');
-		if (percent >= 10) return PhraseManager.getPhrase('rank-bad', 'bad');
+		return PhraseManager.getPhrase('rank-${gradeUntranslated(percent)}', gradeUntranslated(percent));
+	}
 
-		return PhraseManager.getPhrase('rank-awful', 'awful');
+	public function gradeUntranslated(percent:Float = 0.0):String
+	{
+		if (percent == 100) return 'perfect';
+		if (percent >= 90) return 'excellent';
+		if (percent >= 80) return 'great';
+		if (percent >= 60) return 'good';
+		if (percent >= 10) return 'bad';
+
+		return 'awful';
 	}
 }
