1633af971b667ddfdfec2c69c94551c1e36e898d
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-18 20:26:34 -0600

    swapLanguage function Additional: - default_text_content var - TEXT_CONTENT no longer initalized by default - returnLanguageTEXT_CONTENT function

--- source/sap/localization/LocalizationManager.hx
+++ source/sap/localization/LocalizationManager.hx
@@ -1,9 +1,12 @@
 package sap.localization;
 
-class LocalizationManager {
-        public static var LANGUAGE:String = 'english';
+class LocalizationManager
+{
+	public static var LANGUAGE:String = 'english';
 
-        public static var TEXT_CONTENT:Map<String, String> = [
+	public static var TEXT_CONTENT:Map<String, String> = [];
+
+        public static var default_text_content:Map<String, String> = [
                 "hp" => "HP",
                 "dodge" => "DODGE",
                 "rank-perfect" => "perfect",
@@ -20,4 +23,61 @@ class LocalizationManager {
                 "play-continue" => "continue",
                 "play-back" => "back"
         ];
+
+	public static dynamic function swapLanguage()
+	{
+                trace('Trying to swap to languge: $LANGUAGE');
+                
+		TEXT_CONTENT = returnLanguageTEXT_CONTENT();
+
+                if (TEXT_CONTENT == default_text_content && LANGUAGE != 'english')
+                {
+                        trace('Failed to swap to language: $LANGUAGE');
+                } else {
+                        trace('Succeeded to swap to language: $LANGUAGE');
+                }
+	}
+
+	public static dynamic function returnLanguageTEXT_CONTENT():Map<String, String>
+	{
+		if (LANGUAGE == 'spanish')
+			return [
+				"hp" => "SALUD",
+				"dodge" => "ESQUIVAR",
+				"rank-perfect" => "perfecto",
+				"rank-excellent" => "excelente",
+				"rank-great" => "un gran trabajo",
+				"rank-good" => "Bueno",
+				"rank-bad" => "malo",
+				"rank-awful" => "horrible",
+				"you-did" => "LO HICISTE",
+				"menu-play" => "jugar",
+				"menu-credits" => "créditos",
+				"menu-leave" => "dejar",
+				"play-new" => "nuevo",
+				"play-continue" => "continuar",
+				"play-back" => "atrás"
+			];
+
+		if (LANGUAGE == 'portuguese')
+			return [
+				"hp" => "HP",
+				"dodge" => "DESVIE",
+				"rank-perfect" => "perfeito",
+				"rank-excellent" => "excelente",
+				"rank-great" => "ótimo",
+				"rank-good" => "bom",
+				"rank-bad" => "ruim",
+				"rank-awful" => "horrível",
+				"you-did" => "VOCÊ FOI",
+				"menu-play" => "jogar",
+				"menu-credits" => "créditos",
+				"menu-leave" => "sair",
+				"play-new" => "novo",
+				"play-continue" => "continuar",
+				"play-back" => "voltar"
+			];
+
+		return default_text_content;
+	}
 }
