33e522f6a542a1bd3fe3cb8875d3c779a7a921ec
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-18 20:19:24 -0600

    TEXT_CONTENT from english.langhx

--- source/sap/localization/LocalizationManager.hx
+++ source/sap/localization/LocalizationManager.hx
@@ -2,6 +2,22 @@ package sap.localization;
 
 class LocalizationManager {
         public static var LANGUAGE:String = 'english';
-        
-        public static var TEXT_CONTENT:Map<String, String> = [];
+
+        public static var TEXT_CONTENT:Map<String, String> = [
+                "hp" => "HP",
+                "dodge" => "DODGE",
+                "rank-perfect" => "perfect",
+                "rank-excellent" => "excellent",
+                "rank-great" => "great",
+                "rank-good" => "good",
+                "rank-bad" => "bad",
+                "rank-awful" => "awful",
+                "you-did" => "YOU DID",
+                "menu-play" => "play",
+                "menu-credits" => "credits",
+                "menu-leave" => "leave",
+                "play-new" => "new",
+                "play-continue" => "continue",
+                "play-back" => "back"
+        ];
 }
