4454f54a50901495e4da76b052c740dc76bc44f7
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-19 17:28:06 -0600

    More user-friendly python build script :D

--- Build.py
+++ Build.py
@@ -1,16 +1,48 @@
+from queue import Full
 import subprocess
 
 running = True
+dashD_not_required = ['debug' 'watch']
 
 while running:
+        compile_args = ['lime', 'test']
         exit_or_compile = input('Would you like to exit (E) or compile (C)?\n> ')
 
-        if (exit_or_compile.lower() == 'e'):
+        if (exit_or_compile.lower() != 'c'):
                 running = False
                 break
+        else:
+                target_platform = input('What is your target platform?\n> ')
+                
+                if (target_platform == ''):
+                        target_platform = 'hl'
+
+                compile_args.append(target_platform)
+
+                build_flags = input('What are your build flags (seperate with a .)?\n> ')
+                build_flags_array = build_flags.split('.')
+
+                i = 0
+                while i < build_flags_array.__len__():
+
+                        prefix = '-D'
+                        item = build_flags_array[i]
+
+                        if not item == '':
+                                # this doesnt work for some reason.
+                                if dashD_not_required.__contains__(item) == True:
+                                        prefix = '-'
+                                        print(item)
+
+                                compile_args.append(f'{prefix}{item}')
+                        i = i + 1
+
+                print(f'Building for {target_platform} with the build flags: {build_flags_array}')
+        
+        print(compile_args.__str__())
 
         # Execute a simple bash command
-        result = subprocess.run(['lime', 'test', 'hl', '-debug'], capture_output=True, text=True)
+        result = subprocess.run(compile_args, capture_output=True, text=True)
 
         # Check if the command was successful
         if result.returncode == 0:
