a5cb96bfd09ba8f1d27d5e0368494d78cde7d97d
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-19 15:26:26 -0600

    ASSET_SUFFIX variable

--- source/sap/localization/LocalizationManager.hx
+++ source/sap/localization/LocalizationManager.hx
@@ -3,44 +3,51 @@ package sap.localization;
 class LocalizationManager
 {
 	public static var LANGUAGE:String = 'english';
+	public static var ASSET_SUFFIX:String = '';
 
 	public static var TEXT_CONTENT:Map<String, String> = [];
 
-        public static var default_text_content:Map<String, String> = [
-                "hp" => "HP",
-                "dodge" => "DODGE",
-                "rank-perfect" => "perfect",
-                "rank-excellent" => "excellent",
-                "rank-great" => "great",
-                "rank-good" => "good",
-                "rank-bad" => "bad",
-                "rank-awful" => "awful",
-                "you-did" => "YOU DID",
-                "menu-play" => "play",
-                "menu-credits" => "credits",
-                "menu-leave" => "leave",
-                "play-new" => "new",
-                "play-continue" => "continue",
-                "play-back" => "back"
-        ];
+	public static var default_text_content:Map<String, String> = [
+		"hp" => "HP",
+		"dodge" => "DODGE",
+		"rank-perfect" => "perfect",
+		"rank-excellent" => "excellent",
+		"rank-great" => "great",
+		"rank-good" => "good",
+		"rank-bad" => "bad",
+		"rank-awful" => "awful",
+		"you-did" => "YOU DID",
+		"menu-play" => "play",
+		"menu-credits" => "credits",
+		"menu-leave" => "leave",
+		"play-new" => "new",
+		"play-continue" => "continue",
+		"play-back" => "back"
+	];
 
 	public static dynamic function swapLanguage()
 	{
-                trace('Trying to swap to languge: $LANGUAGE');
-                
+		trace('Trying to swap to languge: $LANGUAGE');
+
 		TEXT_CONTENT = returnLanguageTEXT_CONTENT();
 
-                if (TEXT_CONTENT == default_text_content && LANGUAGE != 'english')
-                {
-                        trace('Failed to swap to language: $LANGUAGE');
-                } else {
-                        trace('Succeeded to swap to language: $LANGUAGE');
-                }
+		if (TEXT_CONTENT == default_text_content && LANGUAGE != 'english')
+		{
+                        LANGUAGE = 'english';
+                        ASSET_SUFFIX = '';
+			trace('Failed to swap to language: $LANGUAGE');
+		}
+		else
+		{
+			trace('Succeeded to swap to language: $LANGUAGE');
+		}
 	}
 
 	public static dynamic function returnLanguageTEXT_CONTENT():Map<String, String>
 	{
 		if (LANGUAGE == 'spanish')
+		{
+			ASSET_SUFFIX = 'es';
 			return [
 				"hp" => "SALUD",
 				"dodge" => "ESQUIVAR",
@@ -58,8 +65,11 @@ class LocalizationManager
 				"play-continue" => "continuar",
 				"play-back" => "atrás"
 			];
+		}
 
 		if (LANGUAGE == 'portuguese')
+		{
+			ASSET_SUFFIX = 'pt';
 			return [
 				"hp" => "HP",
 				"dodge" => "DESVIE",
@@ -77,7 +87,9 @@ class LocalizationManager
 				"play-continue" => "continuar",
 				"play-back" => "voltar"
 			];
+		}
 
+		ASSET_SUFFIX = '';
 		return default_text_content;
 	}
 }
