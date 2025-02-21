2892b277a9b3f7309b1cdd9a3a50714f96bec208
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-21 16:49:10 -0600

    Blank settings menu

---
+++ source/sap/settings/SettingsMenu.hx
@@ -0,0 +1,12 @@
+package sap.settings;
+
+class SettingsMenu extends FlxState
+{
+        override function create() {
+                super.create();
+        }
+
+        override function update(elapsed:Float) {
+                super.update(elapsed);
+        }
+}
