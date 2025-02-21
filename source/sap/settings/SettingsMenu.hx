dab8caf82ed32143755a4fddd0ef0a9da77be96b
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-21 17:10:28 -0600

    Basic settings menu

--- source/sap/settings/SettingsMenu.hx
+++ source/sap/settings/SettingsMenu.hx
@@ -1,12 +1,40 @@
 package sap.settings;
 
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import sap.localization.LocalizationManager;
+
 class SettingsMenu extends FlxState
 {
-        override function create() {
-                super.create();
-        }
+	public static var saveValues:Map<String, Any> = ["language" => LocalizationManager.LANGUAGE];
 
-        override function update(elapsed:Float) {
-                super.update(elapsed);
-        }
+	public static var settingsTexts:FlxTypedGroup<FlxText>;
+
+	override function create()
+	{
+		settingsTexts = new FlxTypedGroup<FlxText>();
+		add(settingsTexts);
+
+                var i:Int = 0;
+                for (key in saveValues.keys())
+                {
+                        var keystring:String = Global.getLocalizedPhrase('settings-$key', key);
+                        var keyvalue:Dynamic = saveValues.get(key);
+
+                        var keyText:FlxText = new FlxText(0, 10, 0, '$keystring: $keyvalue', 16);
+                        keyText.screenCenter(X);
+                        keyText.ID = i;
+
+                        settingsTexts.add(keyText);
+
+                        i++;
+                }
+
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
 }
