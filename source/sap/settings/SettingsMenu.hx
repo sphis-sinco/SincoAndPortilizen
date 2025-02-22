d5727d4b8781e7f457129275620f4b6a711337ad
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-21 22:44:34 -0600

    window resolution

--- source/sap/settings/SettingsMenu.hx
+++ source/sap/settings/SettingsMenu.hx
@@ -7,110 +7,158 @@ import sap.mainmenu.MainMenu;
 
 class SettingsMenu extends FlxSubState
 {
+	public static var new_windowres:String;
+
 	public static var saveValues:Map<String, Any> = [];
+	public static var saveValues_array:Array<String> = [];
 	public static var saveValue_length:Int = 0;
+
 	public static var settingsTexts:FlxTypedGroup<FlxText>;
 
-        public static var CURRENT_SELECTION:Int = 0;
-        public static var SELECTED_SETTING:String = "";
-	
-        public static var overlay:BlankBG;
+	public static var CURRENT_SELECTION:Int = 0;
+	public static var SELECTED_SETTING:String = "";
+
+	public static var overlay:BlankBG;
 
 	override function create()
 	{
-                
+                new_windowres = '${FlxG.width}x${FlxG.height}';
+
 		overlay = new BlankBG();
 		overlay.color = 0x000000;
 		overlay.alpha = 0.5;
 		add(overlay);
 
-                saveValuesUpdate();
-                
+		saveValuesUpdate();
+
 		settingsTexts = new FlxTypedGroup<FlxText>();
 		add(settingsTexts);
 
-                createSettingsText();
+		createSettingsText();
 
 		super.create();
 	}
 
-        public static function saveValuesUpdate() {
-                saveValues.set('language', LocalizationManager.LANGUAGE);
-                saveValues.set('volume', FlxG.sound.volume * 100);
+	public static function saveValuesUpdate()
+	{
+		saveValues_array = [];
+                saveValue_length = 0;
 
-                saveValue_length = 2;
-        }
-        
+		newSaveValue('language', LocalizationManager.LANGUAGE);
+		newSaveValue('volume', FlxG.sound.volume * 100);
+
+		#if desktop
+		newSaveValue('window resolution', new_windowres);
+		#end
+	}
+
+	public static function newSaveValue(name:String, value:Any)
+	{
+		saveValues.set(name, value);
+		saveValues_array.push(name);
+		saveValue_length++;
+	}
 
 	override function update(elapsed:Float)
 	{
-                if (FlxG.keys.justReleased.UP) {
-                        CURRENT_SELECTION--;
-                        if (CURRENT_SELECTION < 0) CURRENT_SELECTION = 0;
-                        createSettingsText();
-                }
+		if (FlxG.keys.justReleased.UP)
+		{
+			CURRENT_SELECTION--;
+			if (CURRENT_SELECTION < 0)
+				CURRENT_SELECTION = 0;
+			createSettingsText();
+		}
 
-                if (FlxG.keys.justReleased.DOWN) {
-                        CURRENT_SELECTION++;
-                        if (CURRENT_SELECTION == saveValue_length) CURRENT_SELECTION--;
-                        createSettingsText();
-                }
+		if (FlxG.keys.justReleased.DOWN)
+		{
+			CURRENT_SELECTION++;
+			if (CURRENT_SELECTION == saveValue_length)
+				CURRENT_SELECTION--;
+			createSettingsText();
+		}
 
-                if (FlxG.keys.justReleased.ENTER) {
-                        updateSettings();
-                }
+		if (FlxG.keys.justReleased.ENTER)
+		{
+			updateSettings();
+		}
 
-                if (FlxG.keys.justReleased.ESCAPE) {
+		if (FlxG.keys.justReleased.ESCAPE)
+		{
 			MainMenu.inSubstate = false;
 			close();
-                }
+		}
 
 		super.update(elapsed);
 	}
 
-        public static dynamic function updateSettings() {
-                switch (SELECTED_SETTING)
-                {
-                        case 'language':
-                                LocalizationManager.swapLanguage();
-                                MainMenu.set_menuboxtexts(MainMenu.public_menutextsSelection);
-                        case 'volume':
-                                FlxG.sound.changeVolume(0.1);
-                                if (FlxG.sound.volume == 1)
-                                        FlxG.sound.changeVolume(-1);
-                }
+	public static dynamic function updateSettings()
+	{
+		switch (SELECTED_SETTING)
+		{
+			case 'language':
+				LocalizationManager.swapLanguage();
+				MainMenu.set_menuboxtexts(MainMenu.public_menutextsSelection);
+			case 'volume':
+				FlxG.sound.changeVolume(0.1);
+				if (FlxG.sound.volume == 1)
+					FlxG.sound.changeVolume(-1);
+			case 'window resolution':
+				window_res();
+		}
 
-                saveValuesUpdate();
-                createSettingsText();
-        }
-        
+		saveValuesUpdate();
+		createSettingsText();
+	}
 
-        public static function createSettingsText()
-        {
-                TryCatch.tryCatch(() -> {
-                        for (i in 0...settingsTexts.members.length) {
-                                settingsTexts.members[i].kill();
-                                settingsTexts.remove(settingsTexts.members[0]);
-                        }
-                });
+	static function window_res()
+	{
+		var res:String = saveValues.get(SELECTED_SETTING);
+		switch (res)
+		{
+			case '1280x720':
+				FlxG.resizeWindow(640, 480);
+                                new_windowres = '640x480';
+			case '640x480':
+				FlxG.resizeWindow(640, 608);
+                                new_windowres = '640x608';
+                        case '640x608':
+                                FlxG.resizeWindow(1280, 720);
+                                new_windowres = '1280x720';
+                        default:
+                                FlxG.resizeWindow(1280, 720);
+                                new_windowres = '1280x720';
+		}
+	}
 
-                var i:Int = 0;
-                var cur_y:Float = 10;
-                for (key in saveValues.keys())
-                {
-                        var keystring:String = Global.getLocalizedPhrase('settings-$key', key);
-                        var keyvalue:Dynamic = saveValues.get(key);
+	public static function createSettingsText()
+	{
+		TryCatch.tryCatch(() ->
+		{
+			for (i in 0...settingsTexts.members.length)
+			{
+				settingsTexts.members[i].kill();
+				settingsTexts.remove(settingsTexts.members[0]);
+			}
+		});
 
-                        var keyText:FlxText = new FlxText(10, cur_y, 0, '$keystring: $keyvalue', 16);
-                        keyText.ID = i;
-                        keyText.color = (i == CURRENT_SELECTION) ? 0xFFFF00 : 0xFFFFFF;
-                        cur_y += (i * keyText.size) + 16;
+		var i:Int = 0;
+		var cur_y:Float = 10;
+		for (key in saveValues_array)
+		{
+			var keystring:String = Global.getLocalizedPhrase('settings-$key', key);
+			var keyvalue:Dynamic = saveValues.get(key);
 
-                        if (i == CURRENT_SELECTION) SELECTED_SETTING = key;
+			var keyText:FlxText = new FlxText(10, cur_y, 0, '$keystring: $keyvalue', 16);
+			keyText.ID = i;
+			keyText.color = (i == CURRENT_SELECTION) ? 0xFFFF00 : 0xFFFFFF;
+			cur_y = cur_y + (i + 1 * keyText.size);
 
-                        settingsTexts.add(keyText);
+			if (i == CURRENT_SELECTION)
+				SELECTED_SETTING = key;
 
-                        i++;
-                }
-        }
+			settingsTexts.add(keyText);
+
+			i++;
+		}
+	}
 }
