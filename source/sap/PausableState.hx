77465628bcfa953263177a7378826bef0d821fa1
Sphis-Sinco <sinconsistencia@gmail.com>
2025-02-24 20:20:33 -0600

    Pausable state

---
+++ source/sap/PausableState.hx
@@ -0,0 +1,29 @@
+package sap;
+
+class PausableState extends State
+{
+        public var paused:Bool = false;
+
+	override public function new()
+	{
+		super();
+	}
+
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+                if (FlxG.keys.justReleased.ESCAPE)
+                        togglePaused();
+
+		super.update(elapsed);
+	}
+
+        public function togglePaused()
+        {
+                paused = !paused;
+        }
+}
