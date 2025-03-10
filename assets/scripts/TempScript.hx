3c18a28ef1d85e3edbafcf163b98d38d05a9fde1
Py-than <pythan2025@gmail.com>
2025-03-10 17:46:27 -0500

    Template script

---
+++ assets/scripts/TempScript.hx
@@ -0,0 +1,7 @@
+// used for the gameplay stages and those only
+function preCreate():Void {}
+function postCreate():Void {}
+
+// used in every script
+function create():Void {}
+function update(elapsed:Float):Void {}
