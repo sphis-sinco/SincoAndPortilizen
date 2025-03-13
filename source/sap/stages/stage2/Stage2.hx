b2d9c443723d5e58508119ee6ec40a73174cd296
Sphis-Sinco <sinconsistencia@gmail.com>
2025-03-12 19:05:16 -0500

    Stage 2 sinco is chillin

--- source/sap/stages/stage2/Stage2.hx
+++ source/sap/stages/stage2/Stage2.hx
@@ -2,9 +2,14 @@ package sap.stages.stage2;
 
 class Stage2 extends State
 {
+        public static var sinco:Stage2Sinco;
 
         override function create() {
                 super.create();
+
+                sinco = new Stage2Sinco();
+                add(sinco);
+                sinco.screenCenter();
         }
 
         override function update(elapsed:Float) {
---
+++ source/sap/stages/stage2/Stage2Sinco.hx
@@ -0,0 +1,19 @@
+package sap.stages.stage2;
+
+class Stage2Sinco extends FlxSprite
+{
+
+        override public function new() {
+                super();
+
+                loadGraphic(FileManager.getImageFile('gameplay/sinco stages/Stage2Sinco'), true, 32, 32);
+                animation.add('idle', [0], 0, true);
+                animation.add('fail', [1], 0, false);
+                animation.add('jump', [2], 0, false);
+
+                animation.play('idle');
+
+                Global.scaleSprite(this);
+        }
+        
+}
