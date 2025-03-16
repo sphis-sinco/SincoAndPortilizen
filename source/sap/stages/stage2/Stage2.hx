613f12cbf48ec31c27eeba78ba3fdcec585fe327
Sphis-Sinco <sinconsistencia@gmail.com>
2025-03-16 16:28:53 -0500

    Added the stage 2 background + Changed the stage 2 sinco position

--- source/sap/stages/stage2/Stage2.hx
+++ source/sap/stages/stage2/Stage2.hx
@@ -7,9 +7,17 @@ class Stage2 extends State
         override function create() {
                 super.create();
 
+                var bg:FlxSprite = new FlxSprite().loadGraphic(FileManager.getImageFile('gameplay/sinco stages/Stage2Background'));
+                add(bg);
+                Global.scaleSprite(bg);
+                bg.screenCenter();
+
                 sinco = new Stage2Sinco();
                 add(sinco);
                 sinco.screenCenter();
+
+                sinco.x += 4 * 8;
+                sinco.y += 4 * 24;
         }
 
         override function update(elapsed:Float) {
