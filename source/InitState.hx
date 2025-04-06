c59feabea10ad8c30d84d6419ee839df09fcd758
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-06 11:29:19 -0500

    setupSave on start

--- source/InitState.hx
+++ source/InitState.hx
@@ -81,6 +81,8 @@ class InitState extends FlxState
 	{
                 trace('Proceeding');
 
+                SaveManager.setupSave();
+
 		#if !DISABLE_PLUGINS
 		Plugins.init();
                 trace('Initalizing plugins');
