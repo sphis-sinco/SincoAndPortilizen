80b5fff22478c3ea8f5913f4efccb140a9eabe10
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-11 07:32:20 -0500

    Basic af intro cutscene shit

---
+++ source/sap/stages/sidebit1/Sidebit1IntroCutscene.hx
@@ -0,0 +1,35 @@
+package sap.stages.sidebit1;
+
+import sap.title.TitleState;
+
+class Sidebit1IntroCutscene extends State
+{
+        public static var sprite:SparrowSprite;
+
+        override public function new() {
+              super();
+              
+              sprite = new SparrowSprite('cutscenes/sideline1');
+              sprite.addAnimationByPrefix('part1', 'animation piece 1', 24, false);
+              sprite.addAnimationByPrefix('part2', 'animation piece 2', 24, false);
+              sprite.addAnimationByPrefix('part3', 'animation piece 3', 24, false);
+              sprite.addAnimationByPrefix('part4', 'animation piece 4', 24, false);
+        }
+
+        override function create() {
+                super.create();
+
+                add(sprite);
+                sprite.screenCenter();
+                sprite.playAnimation('part1');
+                sprite.animation.onFinish.add(animName -> {
+                        switch(sprite.animation.name)
+                        {
+                                case 'part1': sprite.playAnimation('part2');
+                                case 'part2': sprite.playAnimation('part3');
+                                case 'part3': sprite.playAnimation('part4');
+                                case 'part4': FlxG.switchState(TitleState.new);
+                        }
+                });
+        }
+}
