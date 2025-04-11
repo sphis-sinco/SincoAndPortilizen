5d2970e77bd509c0b8a5be7106f16985e48f6a46
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-11 15:52:46 -0500

    Cutscene position "editor"

--- source/sap/stages/sidebit1/Sidebit1IntroCutscene.hx
+++ source/sap/stages/sidebit1/Sidebit1IntroCutscene.hx
@@ -4,32 +4,72 @@ import sap.title.TitleState;
 
 class Sidebit1IntroCutscene extends State
 {
-        public static var sprite:SparrowSprite;
+	public static var cutscene_sprite:SparrowSprite;
 
-        override public function new() {
-              super();
-              
-              sprite = new SparrowSprite('cutscenes/sidebit1/pre-sidebit1');
-              sprite.addAnimationByPrefix('part1', 'animation piece 1', 24, false);
-              sprite.addAnimationByPrefix('part2', 'animation piece 2', 24, false);
-              sprite.addAnimationByPrefix('part3', 'animation piece 3', 24, false);
-              sprite.addAnimationByPrefix('part4', 'animation piece 4', 24, false);
-        }
+	override public function new()
+	{
+		super();
 
-        override function create() {
-                super.create();
+		cutscene_sprite = new SparrowSprite('cutscenes/sidebit1/pre-sidebit1');
+		cutscene_sprite.addAnimationByPrefix('part1', 'animation piece 1', 24, false);
+		cutscene_sprite.addAnimationByPrefix('part2', 'animation piece 2', 24, false);
+		cutscene_sprite.addAnimationByPrefix('part3', 'animation piece 3', 24, false);
+		cutscene_sprite.addAnimationByPrefix('part4', 'animation piece 4', 24, false);
+	}
 
-                add(sprite);
-                sprite.screenCenter();
-                sprite.playAnimation('part1');
-                sprite.animation.onFinish.add(animName -> {
-                        switch(sprite.animation.name)
+	override function create()
+	{
+		super.create();
+
+		add(cutscene_sprite);
+		cutscene_sprite.setPosition(-70, 150);
+		cutscene_sprite.playAnimation('part1');
+		cutscene_sprite.animation.onFinish.add(animName ->
+		{
+                        if (SLGame.isDebug)
                         {
-                                case 'part1': sprite.playAnimation('part2');
-                                case 'part2': sprite.playAnimation('part3');
-                                case 'part3': sprite.playAnimation('part4');
-                                case 'part4': FlxG.switchState(TitleState.new);
+                                trace('Automatic cutscene pause');
+                                cutscene_sprite.animation.paused = true;
                         }
-                });
-        }
+
+			switch (cutscene_sprite.animation.name)
+			{
+				case 'part1':
+					cutscene_sprite.x -= 25;
+					cutscene_sprite.y += 50;
+					cutscene_sprite.playAnimation('part2');
+				case 'part2':
+					cutscene_sprite.x = cutscene_sprite.x;
+					cutscene_sprite.y = 0;
+					cutscene_sprite.playAnimation('part3');
+				case 'part3':
+					cutscene_sprite.setPosition(-105, -195);
+					cutscene_sprite.playAnimation('part4');
+				case 'part4':
+					FlxG.switchState(TitleState.new);
+			}
+		});
+	}
+
+	final MOVEMENT_SPEED:Float = 5;
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (cutscene_sprite.animation.paused)
+		{
+			if (FlxG.keys.anyJustReleased([LEFT, RIGHT]))
+				cutscene_sprite.x += (FlxG.keys.justReleased.LEFT) ? -MOVEMENT_SPEED : MOVEMENT_SPEED;
+			if (FlxG.keys.anyJustReleased([UP, DOWN]))
+				cutscene_sprite.y += (FlxG.keys.justReleased.UP) ? -MOVEMENT_SPEED : MOVEMENT_SPEED;
+			if (FlxG.keys.anyJustReleased([LEFT, RIGHT, UP, DOWN]))
+				trace('Cutscene sprite position: ${cutscene_sprite.getPosition()}');
+		}
+
+		if (FlxG.keys.justReleased.SPACE && SLGame.isDebug)
+		{
+			cutscene_sprite.animation.paused = !cutscene_sprite.animation.paused;
+		}
+	}
 }
