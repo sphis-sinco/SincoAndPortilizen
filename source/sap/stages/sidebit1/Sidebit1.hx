d1a3492735ac7b0ffbe3f663f03de2457900ac14
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-12 08:38:26 -0500

    Fix blank sidebit state + sidebit 1 bg

--- source/sap/stages/sidebit1/Sidebit1.hx
+++ source/sap/stages/sidebit1/Sidebit1.hx
@@ -2,36 +2,30 @@ package sap.stages.sidebit1;
 
 class Sidebit1 extends State
 {
-        public static var BACKGROUND_SKY:FlxSprite;
-        public static var BACKGROUND_FLOOR:FlxSprite;
+	public static var DIFFICULTY:String;
+	public static var DIFFICULTY_JSON:Sidebit1DifficultyJson;
 
-        public static var DIFFICULTY:String;
-        public static var DIFFICULTY_JSON:Sidebit1DifficultyJson;
+	override public function new(difficulty:String)
+	{
+		super();
 
-        override public function new(difficulty:String) {
-                super();
-                
 		DIFFICULTY = difficulty;
-		DIFFICULTY_JSON = FileManager.getJSON(FileManager.getDataFile('stages/sidebit1/${difficulty}.json'));
-                trace('Sidebit 1 (${DIFFICULTY})');
-        }
+		DIFFICULTY_JSON = {};
+		trace('Sidebit 1 (${DIFFICULTY})');
+	}
 
-        override function create() {
-                super.create();
+	override function create()
+	{
+		super.create();
+                
+                var bg:FlxSprite = new FlxSprite().loadGraphic(FileManager.getImageFile('gameplay/sidebits/timeVoid'));
+                add(bg);
+                bg.screenCenter();
+                Global.scaleSprite(bg);
+	}
 
-                BACKGROUND_SKY = new FlxSprite();
-                BACKGROUND_SKY.makeGraphic(FlxG.width, FlxG.height, 0xffffff);
-                BACKGROUND_SKY.screenCenter(XY);
-                add(BACKGROUND_SKY);
-
-                BACKGROUND_FLOOR = new FlxSprite();
-                BACKGROUND_FLOOR.makeGraphic(FlxG.width, 64, 0xB5B5B5);
-                BACKGROUND_FLOOR.screenCenter(X);
-                BACKGROUND_FLOOR.y = (FlxG.height - BACKGROUND_FLOOR.height);
-                add(BACKGROUND_FLOOR);
-        }
-
-        override function update(elapsed:Float) {
-                super.update(elapsed);
-        }
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
 }
