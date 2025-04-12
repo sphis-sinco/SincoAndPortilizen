f378487f26eb3b1dcbf70da8f5ef046a446327a0
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-12 08:16:23 -0500

    Sidebit 1 background

--- source/sap/stages/sidebit1/Sidebit1.hx
+++ source/sap/stages/sidebit1/Sidebit1.hx
@@ -2,6 +2,9 @@ package sap.stages.sidebit1;
 
 class Sidebit1 extends State
 {
+        public static var BACKGROUND_SKY:FlxSprite;
+        public static var BACKGROUND_FLOOR:FlxSprite;
+
         public static var DIFFICULTY:String;
         public static var DIFFICULTY_JSON:Sidebit1DifficultyJson;
 
@@ -14,6 +17,17 @@ class Sidebit1 extends State
 
         override function create() {
                 super.create();
+
+                BACKGROUND_SKY = new FlxSprite();
+                BACKGROUND_SKY.makeGraphic(FlxG.width, FlxG.height);
+                BACKGROUND_SKY.screenCenter(XY);
+                add(BACKGROUND_SKY);
+
+                BACKGROUND_FLOOR = new FlxSprite();
+                BACKGROUND_FLOOR.makeGraphic(FlxG.width, 64, 0xB5B5B5);
+                BACKGROUND_FLOOR.screenCenter(X);
+                BACKGROUND_FLOOR.y = FlxG.height - BACKGROUND_FLOOR.height;
+                add(BACKGROUND_FLOOR);
         }
 
         override function update(elapsed:Float) {
