32b5ef051b9817023b0cc462b4dd1bd76276b005
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-12 22:39:08 -0500

    sidebit text & number text

--- source/sap/sidebitmenu/SidebitSelect.hx
+++ source/sap/sidebitmenu/SidebitSelect.hx
@@ -2,9 +2,14 @@ package sap.sidebitmenu;
 
 class SidebitSelect extends State
 {
+	public static var SIDEBIT_NUMBER:Int = 1;
+	public static var SIDEBITS:Int = 1;
+
+	public static var NUMBER_TEXT:SparrowSprite;
+
 	override function create()
 	{
-                Global.playMusic('Lado', 1.0, true);
+		Global.playMusic('Lado', 1.0, true);
 
 		var bg:FlxSprite = new FlxSprite();
 		bg.loadGraphic(FileManager.getImageFile('sidebitmenu/background'));
@@ -18,20 +23,34 @@ class SidebitSelect extends State
 		tv.screenCenter();
 		tv.y += tv.height;
 
-                var side1:FlxSprite = new FlxSprite();
-                side1.loadGraphic(FileManager.getImageFile('sidebitmenu/stage_side'));
-                side1.screenCenter();
-                side1.x += (side1.width / 1.2);
-                side1.y += 35;
-                add(side1);
+		var side1:FlxSprite = new FlxSprite();
+		side1.loadGraphic(FileManager.getImageFile('sidebitmenu/stage_side'));
+		side1.screenCenter();
+		side1.x += (side1.width / 1.2);
+		side1.y += 35;
+		add(side1);
 
-                var side2:FlxSprite = new FlxSprite();
-                side2.loadGraphic(FileManager.getImageFile('sidebitmenu/stage_side'));
-                side2.flipX = true;
-                side2.screenCenter();
-                side2.x -= (side2.width / 1.2);
-                side2.y = side1.y;
-                add(side2);
+		var side2:FlxSprite = new FlxSprite();
+		side2.loadGraphic(FileManager.getImageFile('sidebitmenu/stage_side'));
+		side2.flipX = true;
+		side2.screenCenter();
+		side2.x -= (side2.width / 1.2);
+		side2.y = side1.y;
+		add(side2);
+
+		var sidebitText:FlxSprite = new FlxSprite();
+		sidebitText.loadGraphic(FileManager.getImageFile('sidebitmenu/sidebit'));
+		sidebitText.screenCenter();
+		sidebitText.y += sidebitText.height;
+		add(sidebitText);
+
+		NUMBER_TEXT = new SparrowSprite('sidebitmenu/numbers');
+		NUMBER_TEXT.addAnimationByPrefix('numbers', 'number');
+		NUMBER_TEXT.animation.pause();
+		NUMBER_TEXT.screenCenter();
+		NUMBER_TEXT.y += NUMBER_TEXT.height * 1.4;
+                NUMBER_TEXT.x += 30;
+		add(NUMBER_TEXT);
 
 		super.create();
 	}
@@ -39,5 +58,10 @@ class SidebitSelect extends State
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		if (SIDEBIT_NUMBER > SIDEBITS)
+			SIDEBIT_NUMBER = SIDEBITS;
+
+		NUMBER_TEXT.animation.frameIndex = SIDEBIT_NUMBER - 1;
 	}
 }
