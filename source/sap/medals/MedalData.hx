d739bc158b4b70d82643965229c253105cc40010
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-13 10:38:00 -0500

    flixel_FlxG.save.data.medals.contains is not a function

--- source/sap/medals/MedalData.hx
+++ source/sap/medals/MedalData.hx
@@ -4,24 +4,32 @@ class MedalData
 {
 	public static var unlocked_medals:Array<String> = [];
 
-        public static var cur_y_offset:Float = 0;
+	public static var cur_y_offset:Float = 0;
 
 	public static function unlockMedal(medal:String = 'award'):Medal
 	{
 		var medalClass:Medal = new Medal(medal.replace(' ', '-').replace(',', '').toLowerCase(), unlocked_medals.contains(medal), cur_y_offset);
-                cur_y_offset += 16 * Global.DEFAULT_IMAGE_SCALE_MULTIPLIER;
+		cur_y_offset += 16 * Global.DEFAULT_IMAGE_SCALE_MULTIPLIER;
 
 		trace('New medal: ${medal}');
 
+		#if sys
 		if (FlxG.save.data.medals.contains(medal))
+		#else
+		if (WebSave.MEDALS.contains(medal))
+		#end
 		{
 			trace('MEDAL IS ALREADY EARNED');
 		}
-		else
-		{
-			unlocked_medals.push(medal);
-			FlxG.save.data.medals = unlocked_medals;
-		}
+	else
+	{
+		unlocked_medals.push(medal);
+		#if sys
+		FlxG.save.data.medals = unlocked_medals;
+		#else
+		WebSave.MEDALS = unlocked_medals;
+		#end
+	}
 
 		return medalClass;
 	}
--- source/sap/savedata/WebSave.hx
+++ source/sap/savedata/WebSave.hx
@@ -1,8 +1,8 @@
 package sap.savedata;
 
-class WebSave 
+class WebSave
 {
+	public static var LEVELS_COMPLETE:Array<Int> = [];
 
-        public static var LEVELS_COMPLETE:Array<Int> = [];
-        
+	public static var MEDALS:Array<String> = [];
 }
