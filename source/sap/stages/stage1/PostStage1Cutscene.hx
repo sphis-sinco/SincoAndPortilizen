c9437710b0c8ac7f3d0058e10e11a406b1733c0b
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-13 07:01:37 -0500

    Null Access crash fix

--- source/sap/stages/stage1/PostStage1Cutscene.hx
+++ source/sap/stages/stage1/PostStage1Cutscene.hx
@@ -7,6 +7,7 @@ class PostStage1Cutscene extends PanelCutscene
 	override public function new():Void
 	{
 		super('post-stage1');
+		soundFX = new FlxSound();
 	}
 
 	override function finishedCutscene():Void
@@ -15,13 +16,14 @@ class PostStage1Cutscene extends PanelCutscene
 
 		FlxG.switchState(() -> new Worldmap());
 	}
-        public var soundFX:FlxSound;
+
+	public var soundFX:FlxSound;
 
 	override function panelEvents(panel:Int):Void
 	{
 		super.panelEvents(panel);
 
 		soundFX.loadEmbedded(FileManager.getSoundFile('sounds/poststage1/line-${panel}', CUTSCENES));
-                soundFX.play();
+		soundFX.play();
 	}
 }
