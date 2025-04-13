1d9b3951514de677fd2f6a26ef17abdecd686248
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-13 10:17:22 -0500

    format

--- debug_mods/sincos-cool-mod/scripts/DincoAndLortilizen.hx
+++ debug_mods/sincos-cool-mod/scripts/DincoAndLortilizen.hx
@@ -2,44 +2,50 @@ import funkin.graphics.shaders.AdjustColorShader;
 
 function initalizeMod()
 {
-        TitleState.get_VERSION_TEXT = function():String {
-                return 'Sinco\'s cool mod v1';
-        }
+	TitleState.get_VERSION_TEXT = function():String
+	{
+		return 'Sinco\'s cool mod v1';
+	}
 
-        coolShader = new AdjustColorShader();
-        coolShader.hue = 255;
+	coolShader = new AdjustColorShader();
+	coolShader.hue = 255;
 }
 
 var coolShader:AdjustColorShader;
 
 function statePostCreate()
 {
-        var curState:String = Global.getCurrentState();
+	var curState:String = Global.getCurrentState();
 
-        if (curState == 'TitleState')
-        {
-                TitleState.CHARACTER_RING_CHARS_SHADER.hue = 255;
-                TitleState.MINI_SINCO.shader = coolShader;
-                TitleState.MINI_PORTILIZEN.shader = coolShader;
-        } else if (curState == 'MainMenu')
-        {
-                MainMenu.sinco.shader = coolShader;
-                MainMenu.port.shader = coolShader;
-        } else if (curState == 'State1')
-        {
-                State1.sinco.shader = coolShader;
+	if (curState == 'TitleState')
+	{
+		TitleState.CHARACTER_RING_CHARS_SHADER.hue = 255;
+		TitleState.MINI_SINCO.shader = coolShader;
+		TitleState.MINI_PORTILIZEN.shader = coolShader;
+	}
+	else if (curState == 'MainMenu')
+	{
+		MainMenu.sinco.shader = coolShader;
+		MainMenu.port.shader = coolShader;
+	}
+	else if (curState == 'State1')
+	{
+		State1.sinco.shader = coolShader;
 
-                // lisn
-                State1.osin.shader = coolShader;
-        } else if (curState == 'State2')
-        {
-                State2.sinco.shader = coolShader;
-        } else if (curState == 'State4')
-        {
-                State4.port.shader = coolShader;
-        } else if (curState == 'Sidebit1')
-        {
-                Sidebit1.SINCO.shader = coolShader;
-                Sidebit1.PORTILIZEN.shader = coolShader;
-        }
+		// lisn
+		State1.osin.shader = coolShader;
+	}
+	else if (curState == 'State2')
+	{
+		State2.sinco.shader = coolShader;
+	}
+	else if (curState == 'State4')
+	{
+		State4.port.shader = coolShader;
+	}
+	else if (curState == 'Sidebit1')
+	{
+		Sidebit1.SINCO.shader = coolShader;
+		Sidebit1.PORTILIZEN.shader = coolShader;
+	}
 }
