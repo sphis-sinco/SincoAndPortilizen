f2c5b24ee180edefe494da12af2171c07b1ea563
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-12 22:14:20 -0500

    stage sides + lado (bg music)

--- source/sap/sidebitmenu/SidebitSelect.hx
+++ source/sap/sidebitmenu/SidebitSelect.hx
@@ -4,6 +4,8 @@ class SidebitSelect extends State
 {
 	override function create()
 	{
+                Global.playMusic('Lado', 1.0, true);
+
 		var bg:FlxSprite = new FlxSprite();
 		bg.loadGraphic(FileManager.getImageFile('sidebitmenu/background'));
 		add(bg);
@@ -15,6 +17,21 @@ class SidebitSelect extends State
 		add(tv);
 		tv.screenCenter();
 		tv.y += tv.height;
+
+                var side1:FlxSprite = new FlxSprite();
+                side1.loadGraphic(FileManager.getImageFile('sidebitmenu/stage_side'));
+                side1.screenCenter();
+                side1.x += (side1.width / 1.2);
+                side1.y += 35;
+                add(side1);
+
+                var side2:FlxSprite = new FlxSprite();
+                side2.loadGraphic(FileManager.getImageFile('sidebitmenu/stage_side'));
+                side2.flipX = true;
+                side2.screenCenter();
+                side2.x -= (side2.width / 1.2);
+                side2.y = side1.y;
+                add(side2);
 
 		super.create();
 	}
