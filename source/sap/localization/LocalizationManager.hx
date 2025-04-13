92f1d3050a91c72c86a059119f30efa19fa8d08b
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-13 07:31:05 -0500

    Fix tempo city localization not functioning properly

--- source/sap/localization/LocalizationManager.hx
+++ source/sap/localization/LocalizationManager.hx
@@ -15,7 +15,7 @@ class LocalizationManager
 	public static var default_text_content:Map<String, String> = [
 		"hp" => "HP",
 		"dodge" => "DODGE",
-		"tempo city" => "Tempo city",
+		"tempo-city" => "Tempo city",
 		"rank-perfect" => "perfect",
 		"rank-excellent" => "excellent",
 		"rank-great" => "great",
@@ -63,7 +63,7 @@ class LocalizationManager
 			return [
 				"hp" => "SALUD",
 				"dodge" => "ESQUIVAR",
-                                "tempo city" => "ciudad de ritmo",
+                                "tempo-city" => "ciudad de ritmo",
 				"rank-perfect" => "perfecto",
 				"rank-excellent" => "excelente",
 				"rank-great" => "un gran trabajo",
@@ -90,7 +90,7 @@ class LocalizationManager
 			return [
 				"hp" => "HP",
 				"dodge" => "DESVIE",
-                                "tempo city" => "tempo city",
+                                "tempo-city" => "tempo city",
 				"rank-perfect" => "perfeito",
 				"rank-excellent" => "excelente",
 				"rank-great" => "ótimo",
