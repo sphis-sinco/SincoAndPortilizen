d7d1142155436682b67ca706aef9d9c94688d99c
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-12 22:01:56 -0500

    bg + tv

--- source/sap/sidebitmenu/SidebitSelect.hx
+++ source/sap/sidebitmenu/SidebitSelect.hx
@@ -4,6 +4,18 @@ class SidebitSelect extends State
 {
 	override function create()
 	{
+		var bg:FlxSprite = new FlxSprite();
+		bg.loadGraphic(FileManager.getImageFile('sidebitmenu/background'));
+		add(bg);
+		bg.screenCenter();
+
+		var tv:FlxAnimate = new FlxAnimate(0, 0, 'assets/images/sidebitmenu/tv');
+		tv.anim.addBySymbol('animation', 'static tv', 24, true);
+		tv.anim.play('animation');
+		add(tv);
+		tv.screenCenter();
+		tv.y += tv.height;
+
 		super.create();
 	}
 
