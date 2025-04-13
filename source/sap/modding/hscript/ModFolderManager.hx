fbabb1437cd309d34876221c8e1ec64bc08c0abe
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-13 07:42:46 -0500

    properly read FOLDERS and find valid mods

--- source/sap/modding/hscript/ModFolderManager.hx
+++ source/sap/modding/hscript/ModFolderManager.hx
@@ -2,18 +2,27 @@ package sap.modding.hscript;
 
 class ModFolderManager
 {
-        public static var MODS:Array<String> = [];
+	public static var MODS:Array<String> = [];
 
-        public static var MODS_FOLDER:String = 'mods/';
+	public static var MODS_FOLDER:String = 'mods/';
 
-        public static function readModFolder():Void
-        {
-                // NO DUPES
-                MODS = [];
+	public static function readModFolder():Void
+	{
+		// NO DUPES
+		MODS = [];
 
-                for (folder in FileManager.readDirectory(MODS_FOLDER))
-                {
-                        trace(folder);
-                }
-        }
+		for (folder in FileManager.readDirectory(MODS_FOLDER))
+		{
+			if (FileSystem.isDirectory(folder))
+			{
+				trace('Mod folder: $folder');
+
+				if (FileSystem.readDirectory('${MODS_FOLDER}${folder}').contains('meta.json'))
+				{
+					trace('$folder is a valid mod');
+					MODS.push(folder);
+				}
+			}
+		}
+	}
 }
