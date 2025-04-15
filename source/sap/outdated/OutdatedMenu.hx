5e268fa361b214aa2e732b62e66fa179e4c83afe
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-14 20:04:30 -0500

    text formatting

--- source/sap/outdated/OutdatedMenu.hx
+++ source/sap/outdated/OutdatedMenu.hx
@@ -2,29 +2,31 @@ package sap.outdated;
 
 class OutdatedMenu extends State
 {
-	public static var OUTDATED_TEXT:String = '// ! You are running an outdated version ([VERSION]) ! \\'
-		+ '\n\nIt is recommended to update to the latest version'
-                + '\nespecally if you are working on a mod with script files';
+	public static var OUTDATED_TEXT:String = '-// ! You are running an outdated version ([VERSION]) ! \\\\-'
+		+ '\n\nIt is recommended to update to the latest version (LATEST_VERSION)'
+		+ '\nespecally if you are working on a mod with script files';
 
-        public static var BEGONE:Bool = false;
+	public static var BEGONE:Bool = false;
 
 	override function create()
 	{
 		super.create();
 
-                BEGONE = true;
-                var text:FlxText = new FlxText(0,0,0,OUTDATED_TEXT.replace('[VERSION]', '${Global.VERSION}'),16);
-                text.alignment = CENTER;
-                text.screenCenter();
-                add(text);
+		var textField:FlxText = new FlxText(0, 0, 0, OUTDATED_TEXT.replace('[VERSION]', '${Global.VERSION}').replace('LATEST_VERSION', '${OutdatedCheck.LATEST_VERSION}'), 16);
+		textField.alignment = CENTER;
+		textField.screenCenter();
+		textField.applyMarkup(textField.text, [new FlxTextFormatMarkerPair(new FlxTextFormat(FlxColor.RED, true, true), '-')]);
+		add(textField);
 	}
 
-        override function update(elapsed:Float) {
-                super.update(elapsed);
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
 
-                if (FlxG.keys.justReleased.ENTER)
-                {
-                        InitState.proceed();
-                }
-        }
+		if (FlxG.keys.justReleased.ENTER)
+		{
+			BEGONE = true;
+			InitState.proceed();
+		}
+	}
 }
