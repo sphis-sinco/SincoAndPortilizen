21295474ac8a4dc85796034bd5c9ae5651a37139
Sinco <sinconsistencia@gmail.com>
2025-04-16 04:18:38 -0500

    Proper syntax

--- source/Global.hx
+++ source/Global.hx
@@ -8,8 +8,8 @@ class Global
         public static var RANDOM_STICKER_FOLDERS:Array<String> = ['sinco', 'misc'];
 	// TODO: Softcode this in maybe by reading the images folder and checking the sticker jsons
         public static var RANDOM_STICKER_PACKS:Map<String,String> = [
-		'sinco' -> ['all', 'set-1', 'set-2'],
-		'misc' -> ['all', 'heros', 'villains', 'legendaries']
+		'sinco' => ['all', 'set-1', 'set-2'],
+		'misc' => ['all', 'heros', 'villains', 'legendaries']
 	];
 
         public static function randomStickerFolder():String
