3b5614b6c5beb2581010524610f40f3d6922f46e
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-16 15:29:19 -0500

    flixel.input.keyboard.FlxKey should be Array<flixel.input.keyboard.FlxKey> for function argument 'keys'

--- source/sap/utils/State.hx
+++ source/sap/utils/State.hx
@@ -2,51 +2,56 @@ package sap.utils;
 
 import funkin.ui.transition.StickerSubState;
 
-
 class State extends FlxState
 {
-        public var stickerDegen:Bool = true;
+	public var stickerDegen:Bool = true;
 
 	override public function new():Void
 	{
-                SaveManager.save();
+		SaveManager.save();
 
-                // trace('Crashable state');
+		// trace('Crashable state');
 
 		super();
 
-                ScriptManager.callScript('stateSwitched');
-        }
+		ScriptManager.callScript('stateSwitched');
+	}
 
 	override function create():Void
 	{
 		super.create();
 
-                ScriptManager.callScript('stateCreate');
+		ScriptManager.callScript('stateCreate');
 		postCreate();
 	}
 
-	public function postCreate():Void {
-                ScriptManager.callScript('statePostCreate');
+	public function postCreate():Void
+	{
+		ScriptManager.callScript('statePostCreate');
 
-                if (StickerSubState.grpStickers != null && stickerDegen)
-                {
-                        #if EXCESS_TRACES
-                        trace('Degen stickers');
-                        #end
-                        Global.switchState(this, true);
-                }
-        }
+		if (StickerSubState.grpStickers != null && stickerDegen)
+		{
+			#if EXCESS_TRACES
+			trace('Degen stickers');
+			#end
+			Global.switchState(this, true);
+		}
+	}
 
 	override function update(elapsed:Float):Void
 	{
-                ScriptManager.callScript('stateUpdate', [elapsed]);
+		ScriptManager.callScript('stateUpdate', [elapsed]);
 		super.update(elapsed);
 
-                if (Global.anyKeysPressed(CONTROL, ALT, SHIFT, L))
-                {
-                        ScriptManager.callScript('CASLCrash', [elapsed]);
-                        throw 'CASLCrash';
-                }
+		final controlPressed:Bool = Global.keyPressed(CONTROL);
+		final altPressed:Bool = Global.keyPressed(ALT);
+		final shiftPressed:Bool = Global.keyPressed(SHIFT);
+		final lPressed:Bool = Global.keyPressed(L);
+
+		if (controlPressed && altPressed && shiftPressed && lPressed)
+		{
+			ScriptManager.callScript('CASLCrash', [elapsed]);
+			throw 'CASLCrash';
+		}
 	}
 }
