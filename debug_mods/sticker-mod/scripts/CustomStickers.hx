24e78a3812c788a9066687e2aa1cb10032051ac8
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-16 06:52:39 -0500

    This one line could've fixed the bug.

--- debug_mods/sticker-mod/scripts/CustomStickers.hx
+++ debug_mods/sticker-mod/scripts/CustomStickers.hx
@@ -1,4 +1,5 @@
 function initalizeMod()
 {
         Global.RANDOM_STICKER_FOLDERS = ['stickers-set-1'];
+        Global.RANDOM_STICKER_PACKS = ['stickers-set-1' => ['tutorial', 'parents', 'all']];
 }
