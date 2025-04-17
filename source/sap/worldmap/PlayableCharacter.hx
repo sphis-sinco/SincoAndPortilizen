cfabf93c8dba622a99a06803d3f58710b96b33b2
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-17 06:49:11 -0500

    Using the "results_asset_prefix" field

--- source/sap/worldmap/PlayableCharacter.hx
+++ source/sap/worldmap/PlayableCharacter.hx
@@ -25,9 +25,22 @@ class PlayableCharacterManager
 
 package sap.worldmap;
 
-typedef PlayableCharacter = 
+typedef PlayableCharacter =
 {
-        var character_display_name:String;
-        var levels:Int;
-        var results_asset_prefix:String;
+	var character_display_name:String;
+	var levels:Int;
+
+	var ?results_asset_prefix:String;
+}
+
+class PlayableCharacterManager
+{
+	public static function readPlayableCharacterJSON(character:String):PlayableCharacter
+	{
+		var json:PlayableCharacter = null;
+
+		json = FileManager.getJSON(FileManager.getDataFile('playable_characters/${character}.json'));
+
+		return json;
+	}
 }
