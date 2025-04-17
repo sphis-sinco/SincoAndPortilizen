47d7b5d8c982b3fbe5507076ff509570eb73bd6b
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-17 15:58:55 -0500

    Control management

--- source/sap/worldmap/Worldmap.hx
+++ source/sap/worldmap/Worldmap.hx
@@ -2,51 +2,73 @@ package sap.worldmap;
 
 class Worldmap extends State
 {
-        public static var CURRENT_PLAYER_CHARACTER:String = 'sinco';
-        public static var CURRENT_PLAYER_CHARACTER_JSON:PlayableCharacter = null;
-        public static var CURRENT_PLAYER_LEVELS:Int = 0;
-        public static var CURRENT_PLAYER_SELECTION_OFFSET:Int = 0;
+	public static var CURRENT_PLAYER_CHARACTER:String = 'sinco';
+	public static var CURRENT_PLAYER_CHARACTER_JSON:PlayableCharacter = null;
+	public static var CURRENT_PLAYER_LEVELS:Int = 0;
+	public static var CURRENT_PLAYER_SELECTION_OFFSET:Int = 0;
 
-        public static var CURRENT_SELECTION:Int = 0;
+	public static var CURRENT_SELECTION:Int = 0;
 
-        public static var LEVEL_TEXT:FlxText;
+	public static var LEVEL_TEXT:FlxText;
 
 	override public function new(character:String = 'sinco')
 	{
 		super();
-                
-                CURRENT_PLAYER_CHARACTER = character;
-                CURRENT_PLAYER_CHARACTER_JSON = PlayableCharacterManager.readPlayableCharacterJSON(CURRENT_PLAYER_CHARACTER);
-                CURRENT_PLAYER_LEVELS = CURRENT_PLAYER_CHARACTER_JSON.levels;
-                CURRENT_PLAYER_SELECTION_OFFSET = CURRENT_PLAYER_CHARACTER_JSON.level_number_offset;
+
+		CURRENT_PLAYER_CHARACTER = character;
+		CURRENT_PLAYER_CHARACTER_JSON = PlayableCharacterManager.readPlayableCharacterJSON(CURRENT_PLAYER_CHARACTER);
+		CURRENT_PLAYER_LEVELS = CURRENT_PLAYER_CHARACTER_JSON.levels;
+		CURRENT_PLAYER_SELECTION_OFFSET = CURRENT_PLAYER_CHARACTER_JSON.level_number_offset;
 	}
 
 	override function create()
 	{
 		super.create();
-                
-                CURRENT_SELECTION = 0;
 
-                var background:FlxSprite = new FlxSprite();
-                background.loadGraphic(FileManager.getImageFile('worldmap/worldmapBG'));
-                add(background);
-                Global.scaleSprite(background);
-                background.screenCenter();
+		CURRENT_SELECTION = 0;
 
-                var bar:FlxSprite = new FlxSprite();
-                bar.makeGraphic(FlxG.width, 32 * Global.DEFAULT_IMAGE_SCALE_MULTIPLIER, FlxColor.BLACK);
-                add(bar);
-                bar.screenCenter();
+		var background:FlxSprite = new FlxSprite();
+		background.loadGraphic(FileManager.getImageFile('worldmap/worldmapBG'));
+		add(background);
+		Global.scaleSprite(background);
+		background.screenCenter();
 
-                LEVEL_TEXT = new FlxText(0, 0, 0, 'Hi', 16);
-                add(LEVEL_TEXT);
-                LEVEL_TEXT.screenCenter();
+		var bar:FlxSprite = new FlxSprite();
+		bar.makeGraphic(FlxG.width, 32 * Global.DEFAULT_IMAGE_SCALE_MULTIPLIER, FlxColor.BLACK);
+		add(bar);
+		bar.screenCenter();
+
+		LEVEL_TEXT = new FlxText(0, 0, 0, 'Hi', 16);
+		add(LEVEL_TEXT);
+		LEVEL_TEXT.screenCenter();
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-                LEVEL_TEXT.text = 'Level ${CURRENT_SELECTION + 1 + CURRENT_PLAYER_LEVELS}';
+		LEVEL_TEXT.text = 'Level ${CURRENT_SELECTION + 1 + CURRENT_PLAYER_LEVELS}';
+
+		controlManagement();
+	}
+
+	public static function controlManagement():Void
+	{
+		if (Global.anyKeysJustReleased([UP, DOWN]))
+		{
+			if (Global.keyJustReleased(UP))
+			{
+				CURRENT_SELECTION--;
+				if (CURRENT_SELECTION < 0)
+					CURRENT_SELECTION = 0;
+			}
+			else
+			{
+				CURRENT_SELECTION++;
+
+				if (CURRENT_SELECTION > CURRENT_PLAYER_LEVELS)
+					CURRENT_SELECTION = CURRENT_PLAYER_LEVELS;
+			}
+		}
 	}
 }
