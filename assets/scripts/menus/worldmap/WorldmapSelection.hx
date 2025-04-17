f1b2f4b7ffec77cb61ae66f70788ab3c60fd27c5
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-17 17:29:00 -0500

    Basic level switching ( no difficulty :( )

--- assets/scripts/menus/worldmap/WorldmapSelection.hx
+++ assets/scripts/menus/worldmap/WorldmapSelection.hx
@@ -1,9 +1,16 @@
 function worldmapSelection(character:String, selection:Int)
 {
+        final difficulty:String = 'normal';
+
 	trace(character + ' level ' + selection);
 
 	switch (character)
 	{
+                case 'sinco':
+                        // The reason it's "sap.stage.stage1.Stage1" is to avoid VS Code being an annoying piece of crap.
+                        if (selection == 1) Global.switchState(new sap.stages.stage1.Stage1(difficulty));
+                        else if (selection == 2) Global.switchState(new sap.stages.stage2.Stage2(difficulty));
+
 		default:
 			trace(character + ' has no implementation');
 	}
