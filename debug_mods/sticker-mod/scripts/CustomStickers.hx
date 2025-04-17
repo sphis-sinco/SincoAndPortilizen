510572e3ce94836d99d0d739455181bb748aa8eb
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-17 06:26:39 -0500

    Remove sticker stuff :(

--- debug_mods/sticker-mod/scripts/CustomStickers.hx
+++
@@ -1,5 +0,0 @@
-function initalizeMod()
-{
-        Global.RANDOM_STICKER_FOLDERS = ['stickers-set-1'];
-        Global.RANDOM_STICKER_PACKS = ['stickers-set-1' => ['tutorial', 'parents', 'all']];
-}
