3bbab96eb5f34f2c275f14a6184f6057712d53f1
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-17 18:25:52 -0500

    Reading playable_characters directory to get all the characters

--- source/sap/worldmap/CharacterSelect.hx
+++ source/sap/worldmap/CharacterSelect.hx
@@ -2,6 +2,18 @@ package sap.worldmap;
 
 class CharacterSelect extends State
 {
+        public static var CHARACTER_LIST:Array<String> = [];
+
+        override public function new() {
+                super();
+
+                CHARACTER_LIST = [];
+                for (file in FileManager.readDirectory('assets/data/playable_characters'))
+                {
+                        CHARACTER_LIST.push(file.split('.')[0]);
+                }
+        }
+
 	override function create()
 	{
 		super.create();
