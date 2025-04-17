cd34d0401499f09fa39a057faa77bc55f46229f5
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-17 17:38:59 -0500

    using import has been found to work

--- assets/scripts/menus/worldmap/WorldmapSelection.hx
+++ assets/scripts/menus/worldmap/WorldmapSelection.hx
@@ -1,6 +1,10 @@
+import sap.stages.stage1.Stage1;
+import sap.stages.stage2.Stage2;
+import sap.worldmap.Worldmap;
+
 function worldmapSelection(character:String, selection:Int)
 {
-        final difficulty:String = sap.worldmap.Worldmap.CURRENT_DIFFICULTY;
+        final difficulty:String = Worldmap.CURRENT_DIFFICULTY;
 
 	trace(character + ' level ' + selection);
 
@@ -8,8 +12,8 @@ function worldmapSelection(character:String, selection:Int)
 	{
                 case 'sinco':
                         // The reason it's "sap.stage.stage1.Stage1" is to avoid VS Code being an annoying piece of crap.
-                        if (selection == 1) Global.switchState(new sap.stages.stage1.Stage1(difficulty));
-                        else if (selection == 2) Global.switchState(new sap.stages.stage2.Stage2(difficulty));
+                        if (selection == 1) Global.switchState(new Stage1(difficulty));
+                        else if (selection == 2) Global.switchState(new Stage2(difficulty));
 
 		default:
 			trace(character + ' has no implementation');
