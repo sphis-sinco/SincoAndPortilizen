68925f76477a0dceeca74b9ca03e1446e5afed68
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-17 07:17:57 -0500

    CharSelect file

---
+++ source/sap/worldmap/CharacterSelect.hx
@@ -0,0 +1,14 @@
+package sap.worldmap;
+
+class CharacterSelect extends State
+{
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
