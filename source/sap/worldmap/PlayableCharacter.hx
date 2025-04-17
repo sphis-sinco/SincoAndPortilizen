142dfe81889f5eb32c19108385a303ec7997d0e8
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-17 06:42:52 -0500

    PlayableCharacter JSON

--- source/sap/worldmap/PlayableCharacter.hx
+++ source/sap/worldmap/PlayableCharacter.hx
@@ -22,3 +22,12 @@ class PlayableCharacterManager
 		return json;
 	}
 }
+
+package sap.worldmap;
+
+typedef PlayableCharacter = 
+{
+        var character_display_name:String;
+        var levels:Int;
+        var results_asset_prefix:String;
+}
