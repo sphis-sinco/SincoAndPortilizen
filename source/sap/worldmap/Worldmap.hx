ccf3c2e1422e57832f881e515d027411e547ceae
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-17 06:50:20 -0500

    CURRENT_PLAYER_CHARACTER_JSON

--- source/sap/worldmap/Worldmap.hx
+++ source/sap/worldmap/Worldmap.hx
@@ -3,10 +3,14 @@ package sap.worldmap;
 class Worldmap extends State
 {
         public static var CURRENT_PLAYER_CHARACTER:String = 'sinco';
+        public static var CURRENT_PLAYER_CHARACTER_JSON:PlayableCharacter = null;
 
 	override public function new(character:String = 'sinco')
 	{
 		super();
+                
+                CURRENT_PLAYER_CHARACTER = character;
+                CURRENT_PLAYER_CHARACTER_JSON = PlayableCharacterManager.readPlayableCharacterJSON(CURRENT_PLAYER_CHARACTER);
 	}
 
 	override function create()
