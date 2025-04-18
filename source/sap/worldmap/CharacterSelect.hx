835e5356f259e884fcf1a6134e01c3cb180623c1
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-17 22:39:12 -0500

    Character select box

--- source/sap/worldmap/CharacterSelect.hx
+++ source/sap/worldmap/CharacterSelect.hx
@@ -4,6 +4,8 @@ class CharacterSelect extends State
 {
 	public static var CHARACTER_LIST:Array<String> = [];
 
+	public static var CHARACTER_SELECT_BOX:CharSelector;
+
 	override public function new()
 	{
 		super();
@@ -22,15 +24,38 @@ class CharacterSelect extends State
 
 		var backdrop:FlxSprite = new FlxSprite();
 		backdrop.loadGraphic(FileManager.getImageFile('worldmap/character_select/back'), true, 160, 152);
-                backdrop.animation.add('idle', [0,1], 2);
-                backdrop.animation.play('idle');
+		backdrop.animation.add('idle', [0, 1], 2);
+		backdrop.animation.play('idle');
 		add(backdrop);
 		Global.scaleSprite(backdrop);
 		backdrop.screenCenter();
+
+		CHARACTER_SELECT_BOX = new CharSelector();
+		add(CHARACTER_SELECT_BOX);
+		CHARACTER_SELECT_BOX.screenCenter();
+		CHARACTER_SELECT_BOX.playAnimation('blank');
+                CHARACTER_SELECT_BOX.flipX = true;
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+	}
+}
+
+class CharSelector extends SparrowSprite
+{
+	override public function new()
+	{
+		super('worldmap/character_select/CharSelector');
+
+		addAnimationByPrefix('idle', 'idle', 24, false);
+		addAnimationByPrefix('blank', 'character-box', 24, false);
+		addAnimationByPrefix('select', 'select', 24, false);
+		addAnimationByPrefix('cant-select', 'cant-select', 24, false);
+
+		playAnimation('idle');
+
+		Global.scaleSprite(this, -2);
 	}
 }
