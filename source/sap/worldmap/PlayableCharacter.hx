51a753b58165688e0f04cfbee57dfd0762d825d9
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-18 08:29:33 -0500

    unlocked_when_loaded field

--- source/sap/worldmap/PlayableCharacter.hx
+++ source/sap/worldmap/PlayableCharacter.hx
@@ -3,6 +3,8 @@ package sap.worldmap;
 typedef PlayableCharacter =
 {
 	var character_display_name:String;
+        var unlocked_when_loaded:Bool;
+        
 	var levels:Int;
         var level_number_offset:Int;
 
