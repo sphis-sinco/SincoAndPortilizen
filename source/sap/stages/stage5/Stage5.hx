4ae4deaeb35ab6cfb2d413069ef6171ce19f96f4
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-18 20:05:41 -0500

    STAGE_FIVE build flag implementation

---
+++ source/sap/stages/stage5/Stage5.hx
@@ -0,0 +1,24 @@
+package sap.stages.stage5;
+
+class Stage5 extends State
+{
+	override public function new()
+	{
+		super();
+	}
+
+	override function create()
+	{
+		super.create();
+	}
+
+	override function postCreate()
+	{
+		super.postCreate();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
