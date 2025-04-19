63a4ae5e979367da6dc6db3de485e2d1c7e62cf2
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-19 13:32:04 -0500

    post stage 5 cutscene "voice acting"

--- source/sap/stages/stage5/PostStage5Cutscene.hx
+++ source/sap/stages/stage5/PostStage5Cutscene.hx
@@ -5,16 +5,20 @@ class PostStage5Cutscene extends PanelCutscene
 	override public function new():Void
 	{
 		super('post-stage5');
+		soundFX = new FlxSound();
 	}
+
+	public var soundFX:FlxSound;
 
 	override function panelEvents(panel:Int)
 	{
 		super.panelEvents(panel);
 
 		if (panel == 1)
-		{
 			Global.playSoundEffect('gameplay/attack-failed');
-		}
+
+		soundFX.loadEmbedded(FileManager.getSoundFile('sounds/poststage5/line-${panel}', CUTSCENES));
+		soundFX.play();
 	}
 
 	override function finishedCutscene(?cutscene_skipped:Bool):Void
