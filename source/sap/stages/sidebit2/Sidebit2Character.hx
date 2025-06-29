f5ebcc9fd96bc5cdf348f6c9a568647b04f9a0a7
Sphis-Sinco <sinconsistencia@gmail.com>
2025-06-29 18:55:43 -0500

    Bundle into one class

---
+++ source/sap/stages/sidebit2/Sidebit2Character.hx
@@ -0,0 +1,17 @@
+package sap.stages.sidebit2;
+
+class Sidebit2Character extends SparrowSprite
+{
+
+        override public function new(character:String) {
+                super('gameplay/sidebits/$character-sidebit2');
+
+                addAnimationByPrefix('idle', '$character idle', 24, true);
+                addAnimationByPrefix('hit', '$character hit', 24, false);
+                addAnimationByPrefix('block', '$character block', 24, false);
+                addAnimationByPrefix('punch', '$character punch', 24, false);
+
+                playAnimation('idle');
+        }
+        
+}
