e0a5596c9bdd3c4c129c2273069fcd7d694a22f5
Sphis-Sinco <sinconsistencia@gmail.com>
2025-06-30 12:25:48 -0500

    idle defence down concept got scrapped

--- source/sap/stages/sidebit2/Sidebit2Character.hx
+++ source/sap/stages/sidebit2/Sidebit2Character.hx
@@ -6,7 +6,7 @@ class Sidebit2Character extends SparrowSprite
         override public function new(character:String) {
                 super('gameplay/sidebits/$character-sidebit2');
 
-                addAnimationByPrefix('idle', '$character idle', 24, true);
+                addAnimationByPrefix('idle', '$character idle0', 24, true);
                 addAnimationByPrefix('hit', '$character hit', 24, false);
                 addAnimationByPrefix('block', '$character block', 24, false);
                 addAnimationByPrefix('punch', '$character punch', 24, false);
