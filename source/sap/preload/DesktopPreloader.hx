9348b0ccfe21b1eaed4b905aed5c29362630745f
Sinco <sinconsistencia@gmail.com>
2025-07-01 06:41:39 -0500

    tabs

--- source/sap/preload/DesktopPreloader.hx
+++ source/sap/preload/DesktopPreloader.hx
@@ -17,7 +17,7 @@ class DesktopPreloader extends PreloaderBase
 			trace('Preload progress: ' + '${(currentAssetIndex / assetsToPreload.length) * 100}% ' + '(${currentAssetIndex}/${assetsToPreload.length})');
 
 			#if (target.threaded)
-                         sys.thread.Thread.create(() -> {
+			sys.thread.Thread.create(() -> {
 				 trace('Caching $texturePath in a thread');
 				 Global.cacheTexture(texturePath);
 				 currentAssetIndex++;
