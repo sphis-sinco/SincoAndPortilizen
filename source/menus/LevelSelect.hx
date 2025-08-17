fd13091110a82d9fd0016b3459382ee805937b5a
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-17 18:18:59 -0500

    cursor animations

--- source/menus/LevelSelect.hx
+++ source/menus/LevelSelect.hx
@@ -5,47 +5,56 @@ import flixel.group.FlxGroup.FlxTypedGroup;
 
 class LevelSelect extends FlxState
 {
-        public var cursor:Spr;
+	public var cursor:Spr;
 
-        public var levelIcons:FlxTypedGroup<Spr>;
-        public var levelNames:Array<String> = ['string-quest', 'osin', 'tres']; // tres is Sinco v Port
+	public var levelIcons:FlxTypedGroup<Spr>;
+	public var levelNames:Array<String> = ['string-quest', 'osin', 'tres']; // tres is Sinco v Port
 
 	override function create()
 	{
-                Global.changeDiscordRPCPresence('In the Level Select', null);
+		Global.changeDiscordRPCPresence('In the Level Select', null);
 
 		add(Global.dummyBG([12, 12, 12]));
 
-                levelIcons = new FlxTypedGroup<Spr>();
-                add(levelIcons);
+		levelIcons = new FlxTypedGroup<Spr>();
+		add(levelIcons);
 
-                cursor = new Spr(-3);
-                cursor.loadGraphic(Assets.getImagePath('levelSelect/cursor'), true, 64, 64);
-                cursor.animation.add('idle', [0], 24);
-                cursor.animation.add('select', [1], 24);
-                cursor.animation.play('idle');
-                add(cursor);
+		cursor = new Spr(-3);
+		cursor.loadGraphic(Assets.getImagePath('levelSelect/cursor'), true, 64, 64);
+		cursor.animation.add('idle', [0], 24);
+		cursor.animation.add('select', [1], 24);
+		cursor.animation.play('idle');
+		add(cursor);
 
-                for (i in 0...levelNames.length) {
-                        var levelIcon:Spr = new Spr(-3);
-                        levelIcon.loadGraphic(Assets.getImagePath('levelSelect/level_icons/blank'));
-                        
-                        levelIcon.x = 64 + (i * (128 + 64));
+		for (i in 0...levelNames.length)
+		{
+			var levelIcon:Spr = new Spr(-3);
+			levelIcon.loadGraphic(Assets.getImagePath('levelSelect/level_icons/blank'));
 
-                        levelIcon.screenCenter(Y);
-                        levelIcon.y -= levelIcon.height * 1;
+			levelIcon.x = 64 + (i * (128 + 64));
 
-                        levelIcon.scaleSpr();
+			levelIcon.screenCenter(Y);
+			levelIcon.y -= levelIcon.height * 1;
 
-                        levelIcons.add(levelIcon);
-                }
+			levelIcon.scaleSpr();
+
+			levelIcons.add(levelIcon);
+		}
 
 		super.create();
 	}
 
-        override function update(elapsed:Float) {
-                super.update(elapsed);
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
 
-                cursor.setPosition(FlxG.mouse.x, FlxG.mouse.y);
-        }
+		cursor.setPosition(FlxG.mouse.x, FlxG.mouse.y);
+		cursor.animation.play('idle');
+
+		for (icon in levelIcons.members)
+		{
+			if (cursor.overlaps(icon))
+				cursor.animation.play('select');
+		}
+	}
 }
