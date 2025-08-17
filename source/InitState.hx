be1ec7c9139818e18f55619110eeadd442eb35fc
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-17 18:12:12 -0500

    fix cursor visability crash

--- source/InitState.hx
+++ source/InitState.hx
@@ -40,7 +40,7 @@ class InitState extends FlxState
 
 		super.create();
 
-		FlxG.mouse.cursor.visible = false;
+		FlxG.mouse.visible = false;
 		switchToState(new menus.LevelSelect(), 'Level Select');
 	}
 
