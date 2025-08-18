2993f01cb9d3f4e4a1a6716fbb755dc03126d1ae
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-18 18:18:14 -0500

    sinco jumping

--- source/levels/Osin.hx
+++ source/levels/Osin.hx
@@ -1,5 +1,6 @@
 package levels;
 
+import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.group.FlxGroup.FlxTypedGroup;
 
@@ -10,10 +11,10 @@ class Osin extends PausableState
 
 	public var moving:Bool = false;
 
-        public var osin:Spr;
-        public var sinco:Spr;
+	public var osin:Spr;
+	public var sinco:Spr;
 
-        public var tileY:Float = 0;
+	public var tileY:Float = 0;
 
 	override function create()
 	{
@@ -30,36 +31,36 @@ class Osin extends PausableState
 			spr.y = FlxG.height - spr.height;
 			spr.x = spr.width * i;
 
-                        tileY = spr.y;
+			tileY = spr.y;
 
 			levelTiles.add(spr);
 		}
 
-                osin = new Spr();
-                osin.loadGraphic(Assets.getImagePath('osin/osin'), true, 128, 128);
-                osin.animation.add('idle', [0]);
-                osin.animation.add('prep', [1]);
-                osin.animation.add('attack', [2,3,4], 30, false);
+		osin = new Spr();
+		osin.loadGraphic(Assets.getImagePath('osin/osin'), true, 128, 128);
+		osin.animation.add('idle', [0]);
+		osin.animation.add('prep', [1]);
+		osin.animation.add('attack', [2, 3, 4], 30, false);
 
-                osin.animation.play('idle');
+		osin.animation.play('idle');
 
-                osin.screenCenter();
-                osin.x -= osin.width * 2;
+		osin.screenCenter();
+		osin.x -= osin.width * 2;
 
-                add(osin);
+		add(osin);
 
-                sinco = new Spr();
-                sinco.loadGraphic(Assets.getImagePath('osin/sinco'), true, 128, 128);
-                sinco.animation.add('idle', [0]);
-                sinco.animation.add('jump', [1]);
-                sinco.animation.add('die', [2]);
+		sinco = new Spr();
+		sinco.loadGraphic(Assets.getImagePath('osin/sinco'), true, 128, 128);
+		sinco.animation.add('idle', [0]);
+		sinco.animation.add('jump', [1]);
+		sinco.animation.add('die', [2]);
 
-                sinco.animation.play('idle');
+		sinco.animation.play('idle');
 
-                sinco.screenCenter();
-                sinco.y = tileY - sinco.height;
+		sinco.screenCenter();
+		sinco.y = tileY - sinco.height + 32;
 
-                add(sinco);
+		add(sinco);
 	}
 
 	override function update(elapsed:Float)
@@ -71,6 +72,27 @@ class Osin extends PausableState
 		if (Global.keyJustReleased(SPACE) && !paused && !moving)
 		{
 			moving = true;
+
+			sinco.animation.play('jump');
+
+			FlxTween.tween(sinco, {y: sinco.y - sinco.height * 2}, .5, {
+				ease: FlxEase.sineOut,
+				onComplete: tween ->
+				{
+					FlxTween.tween(sinco, {y: tileY - sinco.height + 32}, .5, {
+						ease: FlxEase.sineIn,
+						onComplete: tween ->
+						{
+							sinco.animation.play('idle');
+						}
+					});
+				},
+				onStart: tween ->
+				{
+					Global.playSoundEffect('gameplay/jump');
+				}
+			});
+
 			for (tile in levelTiles)
 			{
 				FlxTween.tween(tile, {x: tile.x - tile.width}, 1, {
