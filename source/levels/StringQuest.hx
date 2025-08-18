88755f8836242b05cd37b0dab57e27ac47d80f34
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-17 21:57:56 -0500

    block gen

--- source/levels/StringQuest.hx
+++ source/levels/StringQuest.hx
@@ -1,9 +1,32 @@
 package levels;
 
+import flixel.group.FlxGroup.FlxTypedGroup;
+
 class StringQuest extends PausableState
 {
+	public var levelLength:Int = 4;
+	public var levelBlocks:FlxTypedGroup<Spr>;
+
 	override public function new()
 	{
 		super();
+	}
+
+	override function create()
+	{
+		super.create();
+
+		levelBlocks = new FlxTypedGroup<Spr>();
+		add(levelBlocks);
+
+		for (i in 0...levelLength) {
+			var spr:Spr = new Spr();
+			spr.loadGraphic(Assets.getImagePath('string-quest/block'));
+			
+			spr.y = FlxG.height - spr.height;
+			spr.x = spr.width * i;
+			
+			levelBlocks.add(spr);
+		}
 	}
 }
