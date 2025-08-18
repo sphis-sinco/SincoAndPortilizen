e1643ad55ca077aae0f7cc66b12f57d142f29322
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-17 19:45:47 -0500

    testing scale 4 level icons

--- source/menus/LevelSelect.hx
+++ source/menus/LevelSelect.hx
@@ -38,7 +38,7 @@ class LevelSelect extends FlxState
 
 		for (i in 0...levelNames.length)
 		{
-			var levelIcon:Spr = new Spr(-3.25);
+			var levelIcon:Spr = new Spr();
 			levelIcon.loadGraphic(Assets.getImagePath('levelSelect/level_icons/blank'));
 
 			levelIcon.x = (64 + 32) + (i * (128 + 64));
