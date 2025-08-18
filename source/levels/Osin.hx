7d2cb6ebe146457c7f8ccc8b75d95ba83d57e76c
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-18 17:32:07 -0500

    prevent the user breaking it and 1 less tile

--- source/levels/Osin.hx
+++ source/levels/Osin.hx
@@ -5,8 +5,10 @@ import flixel.group.FlxGroup.FlxTypedGroup;
 
 class Osin extends PausableState
 {
-	public var levelLength:Int = 12;
+	public var levelLength:Int = 11;
 	public var levelTiles:FlxTypedGroup<Spr>;
+
+	public var moving:Bool = false;
 
 	override function create()
 	{
@@ -33,15 +35,19 @@ class Osin extends PausableState
 
 		super.update(elapsed);
 
-		if (Global.keyJustReleased(SPACE) && !paused)
+		if (Global.keyJustReleased(SPACE) && !paused && !moving)
 		{
+			moving = true;
 			for (tile in levelTiles)
 			{
 				FlxTween.tween(tile, {x: tile.x - tile.width}, 1, {
 					onComplete: tween ->
 					{
 						if (tile.x <= -tile.width)
-							tile.x = FlxG.width + tile.width;
+							tile.x = FlxG.width;
+
+						if (levelTiles.members[levelTiles.members.length - 1] == tile)
+							moving = false;
 					}
 				});
 			}
