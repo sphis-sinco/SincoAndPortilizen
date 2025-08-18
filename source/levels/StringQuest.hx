86169d248341d1a078654e4b85c6b3ee963b305b
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-17 22:36:28 -0500

    winged enemies

--- source/levels/StringQuest.hx
+++ source/levels/StringQuest.hx
@@ -9,12 +9,17 @@ class StringQuest extends PausableState
 
 	public var port:Spr;
 
+	public var wingedEnemies:FlxTypedGroup<Spr>;
+
 	override function create()
 	{
 		super.create();
 
 		levelBlocks = new FlxTypedGroup<Spr>();
 		add(levelBlocks);
+
+		wingedEnemies = new FlxTypedGroup<Spr>();
+		add(wingedEnemies);
 
 		var lastBlockY:Float = 0;
 		for (i in 0...levelLength)
@@ -30,6 +35,22 @@ class StringQuest extends PausableState
 			lastBlockY = spr.y;
 
 			levelBlocks.add(spr);
+		}
+
+		
+		for (i in 0...5)
+		{
+			var spr:Spr = new Spr();
+			spr.loadGraphic(Assets.getImagePath('string-quest/WingedEnemy'), true, 64, 64);
+
+			spr.animation.add('flap', [0,1], 6);
+			spr.animation.play('flap');
+
+			spr.screenCenter();
+			spr.y -= i * spr.height + 8;
+			spr.x -= i * spr.width + 8;
+
+			wingedEnemies.add(spr);
 		}
 
 		port = new Spr();
