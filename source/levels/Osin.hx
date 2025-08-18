488ed1663b2810dcd6d505e05bd478bd65ed04af
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-18 16:45:47 -0500

    tiles

--- source/levels/Osin.hx
+++ source/levels/Osin.hx
@@ -1,3 +1,50 @@
 package levels;
 
-class Osin extends PausableState {}
+import flixel.tweens.FlxTween;
+import flixel.group.FlxGroup.FlxTypedGroup;
+
+class Osin extends PausableState
+{
+	public var levelLength:Int = 12;
+	public var levelTiles:FlxTypedGroup<Spr>;
+
+	override function create()
+	{
+		super.create();
+
+		levelTiles = new FlxTypedGroup<Spr>();
+		add(levelTiles);
+
+		for (i in 0...levelLength)
+		{
+			var spr:Spr = new Spr();
+			spr.loadGraphic(Assets.getImagePath('osin/tile'));
+
+			spr.y = FlxG.height - spr.height;
+			spr.x = spr.width * i;
+
+			levelTiles.add(spr);
+		}
+	}
+
+	override function update(elapsed:Float)
+	{
+		Global.playMenuMusic();
+
+		super.update(elapsed);
+
+		if (Global.keyJustReleased(SPACE) && !paused)
+		{
+			for (tile in levelTiles)
+			{
+				FlxTween.tween(tile, {x: tile.x - tile.width}, 1, {
+					onComplete: tween ->
+					{
+						if (tile.x <= -tile.width)
+							tile.x = FlxG.width + tile.width;
+					}
+				});
+			}
+		}
+	}
+}
