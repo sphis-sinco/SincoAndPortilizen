25675224a4ef48e70819edd9319d75e9e615fad4
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-17 22:21:06 -0500

    string quest port

--- source/levels/StringQuest.hx
+++ source/levels/StringQuest.hx
@@ -7,6 +7,8 @@ class StringQuest extends PausableState
 	public var levelLength:Int = 20;
 	public var levelBlocks:FlxTypedGroup<Spr>;
 
+	public var port:Spr;
+
 	override function create()
 	{
 		super.create();
@@ -14,6 +16,7 @@ class StringQuest extends PausableState
 		levelBlocks = new FlxTypedGroup<Spr>();
 		add(levelBlocks);
 
+		var lastBlockY:Float = 0;
 		for (i in 0...levelLength)
 		{
 			var spr:Spr = new Spr();
@@ -24,8 +27,23 @@ class StringQuest extends PausableState
 				spr.y -= spr.height;
 			spr.x = spr.width * (i - ((i > 9) ? 10 : 0));
 
+			lastBlockY = spr.y;
+
 			levelBlocks.add(spr);
 		}
+
+		port = new Spr();
+		port.loadGraphic(Assets.getImagePath('string-quest/Port'), true, 64, 64);
+		port.animation.add('run', [0, 1, 2, 3], 6, true);
+		port.animation.add('jump', [4], 24);
+		port.animation.add('fall', [5], 24);
+		port.animation.play('run');
+
+		port.screenCenter(X);
+		port.x += port.width * 2;
+		port.y = lastBlockY - (port.height / 4);
+
+		add(port);
 
 		FlxG.sound.music.stop();
 	}
