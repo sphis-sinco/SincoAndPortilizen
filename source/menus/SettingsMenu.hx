76708b1f2189c273a93b6d737549db49f0f30718
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-18 09:32:41 -0500

    settings menu basically

--- source/menus/SettingsMenu.hx
+++ source/menus/SettingsMenu.hx
@@ -1,3 +1,60 @@
 package menus;
 
-class SettingsMenu extends State {}
+class SettingsMenu extends State
+{
+	public var coloredLevelSelect:Spr;
+
+	public var cursor:Spr;
+	public var selected:Int = -1;
+
+	override function create()
+	{
+		super.create();
+
+		coloredLevelSelect = new Spr();
+		coloredLevelSelect.loadGraphic(Assets.getImagePath('settings/ColoredLevelSelect'), true, 64, 64);
+		coloredLevelSelect.animation.add('false', [0]);
+		coloredLevelSelect.animation.add('true', [1]);
+		coloredLevelSelect.scaleSpr();
+		coloredLevelSelect.setPosition(32, 32);
+		coloredLevelSelect.ID = 0;
+		add(coloredLevelSelect);
+
+		cursor = new Spr(-3);
+		cursor.loadGraphic(Assets.getImagePath('levelSelect/cursor'), true, 64, 64);
+		cursor.animation.add('idle', [0], 24);
+		cursor.animation.add('select', [1], 24);
+		cursor.animation.play('idle');
+		add(cursor);
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		cursor.setPosition(FlxG.mouse.x - (cursor.width / 2), FlxG.mouse.y - (cursor.height / 2));
+		cursor.animation.play('idle');
+
+		coloredLevelSelect.animation.play(Std.string(FlxG.save.data.colored_levelSelect));
+
+		for (setting in [coloredLevelSelect])
+		{
+			setting.scaleSpr();
+			if (cursor.overlaps(setting))
+			{
+				cursor.animation.play('select');
+
+				if (selected != setting.ID)
+				{
+					Global.playSoundEffect('blipSelect');
+                                        selected = setting.ID;
+				}
+
+				setting.scale.set(setting.scale.x - .1, setting.scale.y - .1);
+			}
+
+			if (cursor.animation.name != 'select')
+				selected = -1;
+		}
+	}
+}
