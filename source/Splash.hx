a33cc604d0199167c36c00e6aafa65081e740d81
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-19 10:58:15 -0500

    I DONT WANT U PLS

--- source/Splash.hx
+++ source/Splash.hx
@@ -15,29 +15,35 @@ class Splash extends State
 		super.create();
 
 		splash = new Spr(-2);
-                splash.loadGraphic(Assets.getImagePath('splash'));
+		splash.loadGraphic(Assets.getImagePath('splash'));
 		splash.scaleSpr();
 		splash.screenCenter();
 		add(splash);
 
-                splashText = new FlxText();
-                splashText.text = 'SAP Team';
-                splashText.size = 32;
-                splashText.fieldWidth = FlxG.width;
-                splashText.alignment = 'center';
-                splashText.screenCenter(Y);
-                splashText.y += splashText.height * 4;
-                add(splashText);
+		splashText = new FlxText();
+		splashText.text = 'SAP Team';
+		splashText.size = 32;
+		splashText.fieldWidth = FlxG.width;
+		splashText.alignment = 'center';
+		splashText.screenCenter(Y);
+		splashText.y += splashText.height * 4;
+		add(splashText);
 
 		Global.playSoundEffect('splash');
 
 		FlxTween.tween(splash, {alpha: 0}, 1, {
 			ease: FlxEase.sineInOut,
-                        startDelay: 1
+			startDelay: 1
 		});
 		FlxTimer.wait(2, () ->
 		{
 			InitState.switchToState(new menus.LevelSelect(), 'Level Select');
 		});
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		FlxG.mouse.visible = false;
 	}
 }
