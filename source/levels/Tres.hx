562afdc8deb98f2099c6a1ca61961f821554ed46
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-19 10:50:10 -0500

    sinco and port exist in the tres level now

--- source/levels/Tres.hx
+++ source/levels/Tres.hx
@@ -2,8 +2,25 @@ package levels;
 
 class Tres extends PausableState
 {
+        public var sinco:Spr;
+        public var port:Spr;
+
 	override function create()
 	{
 		super.create();
+
+                sinco = new Spr();
+                sinco.loadGraphic(Assets.getImagePath('tres/superSinco'));
+                sinco.screenCenter();
+                sinco.x -= sinco.width;
+                sinco.y -= sinco.height;
+                add(sinco);
+
+                port = new Spr();
+                port.loadGraphic(Assets.getImagePath('tres/superPort'));
+                port.screenCenter();
+                port.x -= (port.width * 2);
+                port.y += (port.height * 2);
+                add(port);
 	}
 }
