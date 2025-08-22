2ea37b55dac27cf6708b8a9cff5098d5073a29be
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-22 18:03:09 -0500

    mobile stuff

--- source/backend/PausableState.hx
+++ source/backend/PausableState.hx
@@ -110,24 +110,24 @@ class PausableState extends State
 
 			if (icon_left_id != null)
 			{
-				icon_left = new Spr(-2);
+				icon_left = new Spr(#if MOBILE_BUILD (-2) #else (-3) #end);
 				icon_left.loadGraphic(Assets.getImagePath('pauseScreen/icons/$icon_left_id'));
 				icon_left.scaleSpr();
 				icon_left.screenCenter();
-				icon_left.x = icon_left.width / 2;
+				icon_left.x = icon_left.width / #if MOBILE_BUILD 2 #else 10 #end;
 
 				behindPauseButtonLayer.add(icon_left);
 			}
 
 			if (icon_right_id != null)
 			{
-				icon_right = new Spr(-2);
+				icon_right = new Spr(#if MOBILE_BUILD (-2) #else (-3) #end);
 				icon_right.loadGraphic(Assets.getImagePath('pauseScreen/icons/$icon_right_id'));
 				icon_right.flipX = true;
 				icon_right.scaleSpr();
 				icon_right.screenCenter();
 				icon_right.x = FlxG.width;
-				icon_right.x -= icon_right.width * 1.5;
+				icon_right.x -= icon_right.width * #if MOBILE_BUILD 1.5 #else 1 #end;
 
 				behindPauseButtonLayer.add(icon_right);
 			}
